75e31ea5f6a2cd5822b66205434f5150836ce9af
turquoise-hexagon <camille@lzr.pw>
2020-06-11 01:05:12 +0200

    remove temporary variable from euler 113

--- src/problem_113.scm
+++ src/problem_113.scm
@@ -3,8 +3,6 @@
     (let ((b (min b (- a b))))
       (do ((x 1 (add1 x))
            (acc 1 (/ (* acc (+ (- a b) x)) x)))
-;           (acc 1 (let ((tmp (* acc (+ (- a b) x))))
-;                    (/ tmp x))))
         ((> x b) acc))))
   (- (+ (helper (+ 100 10) 10)
         (helper (+ 100  9)  9))
