704da6c6db1f334326606495518619ef9b366e1d
turquoise-hexagon <camille@lzr.pw>
2022-06-20 14:27:51 +0100

    update problem_007.scm

--- src/problem_007/problem_007.scm
+++ src/problem_007/problem_007.scm
@@ -1,12 +1,22 @@
-(import (euler))
+(import
+  (euler))
+
+(define (f x a)
+  (- (/ x (log x)) a))
+
+(define (f-derivative x)
+  (/ (- (log x) 1) (expt (log x) 2)))
+
+(define (f-newton x a)
+  (- x (/ (f x a) (f-derivative x))))
+
+(define (f-inverse a)
+  ;; n / log(x) ~ n^0.85
+  (let loop ((x (expt a 0.85)) (cnt 0))
+    (if (= cnt 10) (inexact->exact (round x))
+      (loop (f-newton x a) (+ cnt 1)))))
 
 (define (solve n)
-  (let solve/h ((i 0) (cnt 0))
-    (let ((cnt (if (prime? i)
-                   (+ cnt 1)
-                   cnt)))
-      (if (= cnt n)
-          i
-          (solve/h (+ i 1) cnt)))))
+  (list-ref (primes (f-inverse n)) n))
 
-(print (solve 10001))
+(print (solve 10000))
