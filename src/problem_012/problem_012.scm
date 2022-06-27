e1af225006d11e08fdc46eddea1e7f449d3428ea
turquoise-hexagon <camille@lzr.pw>
2022-06-27 15:43:18 +0100

    update problem_012

--- src/problem_012/problem_012.scm
+++ src/problem_012/problem_012.scm
@@ -1,9 +1,10 @@
-(import (euler))
+(import
+  (euler))
 
 (define (solve n)
-  (let solve/h ((a 1) (b 2))
+  (let loop ((a 1) (b 2))
     (if (> (length (divisors a)) n)
-        a
-        (solve/h (+ a b) (+ b 1)))))
+      a
+      (loop (+ a b) (+ b 1)))))
 
 (print (solve 500))
