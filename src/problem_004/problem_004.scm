16d483c0a33991b23e1b3ec41a0294dd9da8d774
turquoise-hexagon <camille@lzr.pw>
2022-06-27 13:34:49 +0100

    update problem_004

--- src/problem_004/problem_004.scm
+++ src/problem_004/problem_004.scm
@@ -1,17 +1,16 @@
-(import (euler)
-        (srfi 1))
+(import
+  (euler)
+  (srfi 1))
 
 (define (solve n)
-  (let* ((low (expt 10 (- n 1))) (high (expt 10 n)) (lst (iota (- high low) low)))
-    (apply max (fold
-                 (lambda (a acc)
-                   (append (fold
-                             (lambda (b acc)
-                               (if (palindrome? (* a b))
-                                   (cons (* a b) acc)
-                                   acc))
-                             '() lst)
-                           acc))
-                 '() lst))))
+  (apply max
+    (filter palindrome?
+      (map
+        (lambda (pair)
+          (apply * pair))
+        (powerset
+          (range (expt 10 (- n 1))
+                 (- (expt 10 n) 1))
+          2)))))
 
 (print (solve 3))
