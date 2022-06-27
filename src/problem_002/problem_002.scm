0a1c030b7f5d89b8eb9504f0ef8e736c64470750
turquoise-hexagon <camille@lzr.pw>
2022-06-27 13:34:33 +0100

    update problem_002

--- src/problem_002/problem_002.scm
+++ src/problem_002/problem_002.scm
@@ -1,9 +1,10 @@
 (define (solve n)
-  (let solve/h ((a 0) (b 1) (acc 0))
+  (let loop ((a 0) (b 1) (acc 0))
     (if (> a n)
-        acc
-        (solve/h b (+ a b) (if (even? a)
-                              (+ acc a)
-                              acc)))))
+      acc
+      (loop b (+ a b)
+        (if (even? a)
+          (+ acc a)
+          acc)))))
 
 (print (solve 4000000))
