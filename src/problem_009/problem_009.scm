5c29e2753d8294e55f3d0094ff9eb4feaf5b8223
turquoise-hexagon <camille@lzr.pw>
2022-06-27 13:35:50 +0100

    update problem_009

--- src/problem_009/problem_009.scm
+++ src/problem_009/problem_009.scm
@@ -1,12 +1,10 @@
-(import (srfi 1))
-
 (define (solve n)
   (call/cc
-    (lambda (return)
-      (do ((i 1 (+ i 1))) ((= i n))
-        (do ((j i (+ j 1))) ((= j n))
-          (do ((k j (+ k 1))) ((= k n))
-            (when (and (= (+ (* i i) (* j j)) (* k k)) (= (+ i j k) n))
-              (return (* i j k)))))))))
-  
+    (lambda (_)
+      (do ((i 1 (+ i 1))) ((>= i (/ n 3)))
+        (do ((j i (+ j 1))) ((>= j (/ n 2)))
+          (let ((k (- n i j)))
+            (when (= (+ (* i i) (* j j)) (* k k))
+              (_ (* i j k)))))))))
+
 (print (solve 1000))
