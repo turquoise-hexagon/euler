149436dc72a8bcbefa95bb444a5e7c6a4014a3a8
turquoise-hexagon <camille@lzr.pw>
2022-06-27 16:55:45 +0100

    update problem_014

--- src/problem_014/problem_014.scm
+++ src/problem_014/problem_014.scm
@@ -1,19 +1,39 @@
-(import (srfi 1))
+(import
+  (srfi 69))
+
+(define _collatz (make-hash-table))
+
+(define (helper! start lst)
+  (foldl
+    (lambda (cur i)
+      (hash-table-set! _collatz i cur)
+      (+ cur 1))
+    start lst))
 
 (define (collatz n)
-  (let collatz/h ((n n) (acc (list)))
-    (let ((acc (cons n acc)))
-      (if (= n 1)
-          acc
-          (collatz/h (if (even? n) (quotient n 2) (+ (* 3 n) 1)) acc)))))
+  (let loop ((i n) (acc '()))
+    (let ((acc (cons i acc)))
+      (if (hash-table-exists? _collatz i)
+        (let ((tmp (hash-table-ref _collatz i)))
+          (helper! tmp acc)
+          (+ tmp (length acc)))
+        (if (= i 1)
+          (begin
+            (helper! 1 acc)
+            (length acc))
+          (loop
+            (if (even? i)
+              (quotient i 2)
+              (+ (* 3 i) 1))
+            acc))))))
 
 (define (solve n)
-  (cdr (fold
-         (lambda (a acc)
-           (let ((tmp (length (collatz a))))
-             (if (> tmp (car acc))
-                 (cons tmp a)
-                 acc)))
-         '(0 . 0) (iota n 1))))
+  (let loop ((i 1) (len 0) (acc 0))
+    (if (> i n)
+      acc
+      (let ((tmp (collatz i)))
+        (if (> tmp len)
+          (loop (+ i 1) tmp i)
+          (loop (+ i 1) len acc))))))
 
 (print (solve 1000000))
