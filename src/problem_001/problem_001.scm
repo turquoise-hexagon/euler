6a27069adc5128ece6ae148f46a1baa3ec9c9fa2
turquoise-hexagon <camille@lzr.pw>
2022-06-27 13:34:20 +0100

    update problem_001

--- src/problem_001/problem_001.scm
+++ src/problem_001/problem_001.scm
@@ -1,10 +1,11 @@
-(import (srfi 1))
-
 (define (solve n)
-  (apply + (filter
-             (lambda (i)
-               (or (= 0 (modulo i 3))
-                   (= 0 (modulo i 5))))
-             (iota n))))
+  (let loop ((i 1) (acc 0))
+    (if (>= i n)
+      acc
+      (loop (+ i 1)
+        (if (or (= (modulo i 3) 0)
+                (= (modulo i 5) 0))
+          (+ acc i)
+          acc)))))
 
 (print (solve 1000))
