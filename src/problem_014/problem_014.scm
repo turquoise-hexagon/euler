354d2f6b517b853be25e56164cb6730f7a0dfc97
turquoise-hexagon <camille@lzr.pw>
2022-06-28 15:11:15 +0100

    update problem_014

--- src/problem_014/problem_014.scm
+++ src/problem_014/problem_014.scm
@@ -7,17 +7,17 @@
 (define (collatz n)
   (let loop ((i n) (acc 0))
     (if (hash-table-exists? _collatz i)
-      (let ((tmp (hash-table-ref _collatz i)))
-        (hash-table-set! _collatz n (+ tmp acc))
-        (+ tmp acc))
+      (let ((tmp (+ (hash-table-ref _collatz i) acc)))
+        (hash-table-set! _collatz n tmp)
+        tmp)
       (loop
         (if (even? i)
-         (/ i 2)
-         (+ (* 3 i) 1))
+          (quotient i 2)
+          (+ (* 3 i) 1))
         (+ acc 1)))))
 
 (define (solve n)
-  (let loop ((i 1) (len 0) (acc 0))
+  (let loop ((i 2) (len 0) (acc 0))
     (if (> i n)
       acc
       (let ((tmp (collatz i)))
