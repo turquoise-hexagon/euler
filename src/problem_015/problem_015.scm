2d6ab77b27027d1b7ce959f283f23472dc6e780c
turquoise-hexagon <camille@lzr.pw>
2022-06-28 12:22:18 +0100

    update problem_015

--- src/problem_015/problem_015.scm
+++ src/problem_015/problem_015.scm
@@ -1,9 +1,10 @@
-(import (euler))
+(import
+  (euler))
 
 (define (binomial-coefficient a b)
   (/ (factorial a) (* (factorial b) (factorial (- a b)))))
 
 (define (solve n)
-  (binomial-coefficient (+ n n) n))
+  (binomial-coefficient (* 2 n) n))
 
 (print (solve 20))
