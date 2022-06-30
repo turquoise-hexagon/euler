18ec0e6c8a15639b29da52d11a32b99599a9c8eb
turquoise-hexagon <camille@lzr.pw>
2022-06-30 17:34:15 +0100

    update problem_025

--- src/problem_025/problem_025.scm
+++ src/problem_025/problem_025.scm
@@ -2,9 +2,9 @@
   (string-length (number->string n)))
 
 (define (solve n)
-  (let solve/h ((a 0) (b 1) (acc 0))
+  (let loop ((a 0) (b 1) (acc 0))
     (if (= (number-length a) n)
-        acc
-        (solve/h b (+ a b) (+ acc 1)))))
+      acc
+      (loop b (+ a b) (+ acc 1)))))
 
 (print (solve 1000))
