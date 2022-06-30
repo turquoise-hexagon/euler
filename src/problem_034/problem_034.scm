b9d9f1d5b65f56af2de5e8509ddbd8293f556a23
turquoise-hexagon <camille@lzr.pw>
2022-06-30 21:51:20 +0100

    update problem_034

--- src/problem_034/problem_034.scm
+++ src/problem_034/problem_034.scm
@@ -1,15 +1,29 @@
-(import (euler)
-        (srfi 1))
+(import
+  (euler)
+  (srfi 1))
 
 (define factorials
-  (map factorial (iota 10)))
+  (list->vector
+    (map factorial (range 0 9))))
 
-(define (is-valid? n)
-  (= n (apply + (map
-                  (cut list-ref factorials <>)
-                  (number->list n)))))
+(define upper-bound
+  (let ((val (factorial 9)))
+    (let loop ((i 1))
+      (let ((tmp (expt 10 i)))
+        (if (> tmp (* i val))
+          tmp
+          (loop (+ i 1)))))))
+
+(define (valid? n)
+  (=
+    (apply +
+      (map
+        (lambda (i)
+          (vector-ref factorials i))
+        (number->list n)))
+    n))
 
 (define (solve)
-  (- (apply + (filter is-valid? (iota 1000000))) 3))
+  (apply + (filter valid? (range 3 upper-bound))))
 
 (print (solve))
