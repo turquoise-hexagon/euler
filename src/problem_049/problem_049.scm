602bea2263cc9037011447859988fe0adec0f314
turquoise-hexagon <camille@lzr.pw>
2022-07-12 15:11:28 +0100

    add problem_049

---
+++ src/problem_049/problem_049.scm
@@ -0,0 +1,38 @@
+(import
+  (chicken sort)
+  (euler)
+  (srfi 1))
+
+(define (helper number start stop)
+  (filter
+    (lambda (i)
+      (and (<= start i stop) (prime? i)))
+    (delete-duplicates
+      (map list->number (permutations (number->list number)))
+      =)))
+
+(define (generate-sequences number start stop length-sequence)
+  (let ((lst (helper number start stop)))
+    (if (>= (length lst) length-sequence)
+      (filter
+        (lambda (i)
+          (apply = (map - i (cdr i))))
+        (powerset (sort lst <) length-sequence))
+      '())))
+
+(define (convert-to-output lst)
+  (list->number (join (map number->list lst))))
+
+(define (solve number-digits length-sequence)
+  (let ((start (expt 10 (- number-digits 1))) (stop (- (expt 10 number-digits) 1)))
+    (convert-to-output
+      (second
+        (delete-duplicates
+          (join
+            (filter-map
+              (lambda (i)
+                (generate-sequences i start stop length-sequence))
+              (filter prime? (range start stop))))
+          equal?)))))
+
+(print (solve 4 3))
