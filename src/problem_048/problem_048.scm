924f884053f0278740afa083121766290b3cec1e
turquoise-hexagon <camille@lzr.pw>
2022-07-12 14:11:31 +0100

    update problem_048

--- src/problem_048/problem_048.scm
+++ src/problem_048/problem_048.scm
@@ -1,6 +1,7 @@
-(import (srfi 1))
+(import
+  (euler))
 
 (define (solve n)
-  (modulo (apply + (map (lambda (i) (expt i i)) (iota 1000 1))) (expt 10 n)))
+  (modulo (apply + (map (lambda (i) (expt i i)) (range 1 1000))) (expt 10 n)))
 
 (print (solve 10))
