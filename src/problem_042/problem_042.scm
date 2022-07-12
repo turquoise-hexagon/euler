8543cf0e285b4093c45ca8b8e99ab2ce22b4bdf2
turquoise-hexagon <camille@lzr.pw>
2022-07-12 12:06:34 +0100

    update problem_042

--- src/problem_042/problem_042.scm
+++ src/problem_042/problem_042.scm
@@ -1,26 +1,25 @@
-(import (chicken io)
-        (chicken string)
-        (srfi 1))
+(import
+  (chicken io)
+  (chicken string)
+  (euler)
+  (srfi 1))
 
 (define (import-input)
-  (string-split (read-line) "\","))
-
-(define (get-value str)
   (let ((base (char->integer #\A)))
-    (apply + (map
-               (lambda (i)
-                 (- (char->integer i) base -1))
-               (string->list str)))))
+    (map
+      (lambda (str)
+        (apply +
+          (map
+            (lambda (i)
+              (- (char->integer i) base -1))
+            (string->list str))))
+      (string-split (read-line) "\","))))
 
-(define (triangle n)
-  (* 1/2 n (+ n 1)))
-
-(define (triangles n)
-  (map triangle (iota n)))
+(define (triangle? n)
+  (integer? (sqrt (+ (* 8 n) 1))))
 
 (define (solve input)
-  (let ((triangles (triangles 100)))
-    (count (cut member <> triangles) (map get-value input))))
+  (count triangle? input))
 
 (let ((input (import-input)))
   (print (solve input)))
