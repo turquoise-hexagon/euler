5e76d9e21b916af7ba14c3a0dec815c99396d385
turquoise-hexagon <camille@lzr.pw>
2022-07-12 13:53:50 +0100

    add problem_046

---
+++ src/problem_046/problem_046.scm
@@ -0,0 +1,22 @@
+(import
+  (euler)
+  (srfi 69))
+
+(define limit 5000)
+
+(define (generate)
+  (let ((acc (make-hash-table)) (lst (range 0 limit)))
+    (for-each
+      (lambda (p)
+        (for-each (lambda (_) (hash-table-set! acc (+ p (* 2 _ _)) #t)) lst))
+      (primes limit))
+    acc))
+
+(define (solve)
+  (let ((table (generate)))
+    (let loop ((i 3))
+      (if (hash-table-exists? table i)
+        (loop (+ i 2))
+        i))))
+
+(print (solve))
