8027aa07862dd52ae69c1d32799d26c349aab760
turquoise-hexagon <camille@lzr.pw>
2022-07-18 03:45:57 +0100

    update problem_052

--- src/problem_052/problem_052.scm
+++ src/problem_052/problem_052.scm
@@ -1,20 +1,22 @@
-(import (chicken sort)
-        (euler)
-        (srfi 1))
+(import
+  (chicken sort)
+  (euler)
+  (srfi 1))
 
-(define (generate-key n)
-  (sort (number->list n) <))
+(define (make-valid? n)
+  (let ((muls (range 1 n)))
+    (lambda (i)
+      (let ((tmp (map
+                   (lambda (mul)
+                     (sort (number->list (* i mul)) <))
+                   muls)))
+        (every equal? tmp (cdr tmp))))))
 
-(define (valid? n muls)
-  (let ((lst (generate-key n)))
-    (foldl
-      (lambda (acc a)
-        (and (equal? (generate-key (* n a)) lst) acc))
-      #t muls)))
+(define (solve n)
+  (let ((valid? (make-valid? n)))
+    (let loop ((i 1))
+      (if (valid? i)
+        i
+        (loop (+ i 1))))))
 
-(define (solve muls)
-  (let solve/1/h ((i 1))
-    (if (valid? i muls) i
-        (solve/1/h (+ i 1)))))
-
-(print (solve '(2 3 4 5 6)))
+(print (solve 6))
