86f33bf67321e47e0081cb9cf9fe0bc8f247fa22
turquoise-hexagon <camille@lzr.pw>
2022-07-18 17:18:44 +0100

    update problem_056

--- src/problem_056/problem_056.scm
+++ src/problem_056/problem_056.scm
@@ -1,10 +1,11 @@
-(import (euler)
-        (srfi 1))
+(import
+  (euler))
 
 (define (solve n)
-  (apply max (map
-               (lambda (lst)
-                 (digitsum (apply (cut expt <> <>) lst)))
-               (combinations (iota n) 2))))
+  (apply max
+    (map
+      (lambda (pairs)
+        (digitsum (apply expt pairs)))
+      (combinations (range 1 n) 2))))
 
 (print (solve 100))
