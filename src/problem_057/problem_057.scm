87b7957fd137255aaf74d58812aebda37325066f
turquoise-hexagon <camille@lzr.pw>
2022-07-19 12:47:17 +0100

    update problem_057

--- src/problem_057/problem_057.scm
+++ src/problem_057/problem_057.scm
@@ -1,12 +1,18 @@
-(import (euler))
+(import
+  (euler))
+
+(define (number-length n)
+  (length (number->list n)))
 
 (define (solve n)
-  (let solve/h ((num 3) (den 2) (i 0) (acc 0))
-    (if (> i n) acc
-        (let* ((num (+ num den den)) (den (- num den)))
-          (solve/h num den (+ i 1) (if (> (length (number->list num))
-                                          (length (number->list den)))
-                                       (+ acc 1)
-                                       acc))))))
+  (let loop ((num 3) (den 2) (i 0) (acc 0))
+    (if (> i n)
+      acc
+      (let* ((num (+ num den den)) (den (- num den)))
+        (loop num den (+ i 1)
+          (if (> (number-length num)
+                 (number-length den))
+            (+ acc 1)
+            acc))))))
 
 (print (solve 1000))
