3e6b1b9b29d0bb94ef1f2598d51970a9c31b49d4
turquoise-hexagon <camille@lzr.pw>
2022-07-19 23:59:21 +0100

    add problem_033

---
+++ src/problem_033/problem_033.scm
@@ -0,0 +1,22 @@
+(import
+  (euler)
+  (srfi 1))
+
+(define (valid? a b)
+  (if (= (remainder a 10)
+         (quotient  b 10))
+    (let ((q (quotient  a 10))
+          (r (remainder b 10)))
+      (if (= r 0)
+        #f
+        (= (/ a b) (/ q r))))
+    #f))
+
+(define (solve)
+  (let ((_ (filter
+             (lambda (pair)
+               (apply valid? pair))
+             (combinations (range 10 99) 2))))
+    (denominator (apply / (apply map * _)))))
+
+(print (solve))
