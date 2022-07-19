a22e4586bbc2c5c8893eb163e174937ca98ac43d
turquoise-hexagon <camille@lzr.pw>
2022-07-19 13:06:39 +0100

    add problem_058

---
+++ src/problem_058/problem_058.scm
@@ -0,0 +1,18 @@
+(import
+  (euler)
+  (srfi 1))
+
+(define (diagonals n)
+  `(,(+ (* 4 n n) (* 4 n)  1)
+    ,(+ (* 4 n n) (* 2 n)  1)
+    ,(- (* 4 n n) (* 2 n) -1)
+    ,(+ (* 4 n n) 1)))
+
+(define (solve)
+  (let loop ((i 1) (primes 0) (total 1))
+    (let ((primes (+ primes (count prime? (diagonals i)))) (total (+ total 4)))
+      (if (> 1/10 (/ primes total))
+        (+ (* 2 i) 1)
+        (loop (+ i 1) primes total)))))
+
+(print (solve))
