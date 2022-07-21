65e73b28a718acb0cf224ede498b17118a57fcbf
turquoise-hexagon <camille@lzr.pw>
2022-07-21 22:56:24 +0100

    add problem_065

---
+++ src/problem_065/problem_065.scm
@@ -0,0 +1,15 @@
+(import
+  (euler))
+
+(define (solve n)
+  (let loop ((i 2) (tmp 1) (acc 2))
+    (if (> i n)
+      (digitsum acc)
+      (loop (+ i 1) acc
+        (+ (* (if (= (modulo i 3) 0)
+                (* 2 (/ i 3))
+                1)
+             acc)
+           tmp)))))
+
+(print (solve 100))
