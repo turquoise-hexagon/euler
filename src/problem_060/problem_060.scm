e5791d0559be9ce5a0df81f7b88685f660b08378
turquoise-hexagon <camille@lzr.pw>
2022-07-21 22:07:09 +0100

    add problem_060

---
+++ src/problem_060/problem_060.scm
@@ -0,0 +1,43 @@
+(import
+  (euler)
+  (only (srfi 1)
+    filter
+    every))
+
+(define (concatenate a b)
+  (let loop ((i b) (a a))
+    (if (= i 0)
+      (+ a b)
+      (loop (quotient i 10) (* a 10)))))
+
+(define (group acc primes)
+  (foldl
+    (lambda (acc prime)
+      (map
+        (lambda (group)
+          (if (every
+                (lambda (i)
+                  (and (prime? (concatenate prime i))
+                       (prime? (concatenate i prime))))
+                group)
+            (cons prime group)
+            group))
+        acc))
+    acc primes))
+
+(define (solve n)
+  (let ((primes (primes 10000)))
+    (let loop ((acc (map list primes)))
+      (let ((tmp (group acc primes)))
+        (if (equal? tmp acc)
+          (apply min
+            (map
+              (lambda (group)
+                (apply + group))
+              (filter
+                (lambda (group)
+                  (= (length group) n))
+                acc)))
+          (loop tmp))))))
+
+(print (solve 5))
