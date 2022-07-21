3cd9083d3f9b1cda6b8ca3d57953284636f2ad8a
turquoise-hexagon <camille@lzr.pw>
2022-07-22 00:32:51 +0100

    update problem_079

--- src/problem_079/problem_079.scm
+++ src/problem_079/problem_079.scm
@@ -1,46 +1,53 @@
-(import (chicken io)
-        (matchable)
-        (srfi 69)
-        (srfi 1))
+(import
+  (chicken io)
+  (srfi 69)
+  (srfi 1))
 
-(define (import-input)
-  (map string->list (read-lines)))
-
-(define (graph input)
-  (define (graph/h hash key value)
-    (let ((query (hash-table-ref/default hash key '())))
-      (unless (member value query) (hash-table-set! hash key (cons value query)))))
-  (let ((hash (make-hash-table)))
-    (for-each
-      (match-lambda
-        ((a b c)
-         (graph/h hash a b)
-         (graph/h hash b c)))
-      input)
-    hash))
-
-(define (uniq hash)
-  (hash-table-fold hash
-    (lambda (key val acc)
-      (if (= (length val) 1)
-          (cons key val)
-          acc))
+(define (add-connection! graph a b)
+  (hash-table-update!/default graph a
+    (lambda (current)
+      (if (not (member b current))
+        (cons b current)
+        current))
     '()))
 
-(define (path hash)
-  (let ((keys (hash-table-keys hash)))
-    (let path/h ((acc '()) (last 0))
+(define (graph attempts)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (attempt)
+        (apply
+          (lambda (a b c)
+            (add-connection! acc a b)
+            (add-connection! acc b c))
+          attempt))
+      attempts)
+    acc))
+
+(define (import-input)
+  (graph (map string->list (read-lines))))
+
+(define (single graph)
+  (let loop ((keys (hash-table-keys graph)))
+    (let ((_ (hash-table-ref graph (car keys))))
+      (if (= (length _) 1)
+        (cons (car keys) _)
+        (loop (cdr keys))))))
+
+(define (path graph)
+  (let ((keys (hash-table-keys graph)))
+    (let loop ((last 0) (acc '()))
       (if (= (length acc) (length keys))
-          (cons last acc)
-          (match (uniq hash)
-            ((key val)
-             (for-each
-               (lambda (i)
-                 (hash-table-set! hash i (delete val (hash-table-ref hash i))))
-               keys)
-             (path/h (cons val acc) key)))))))
+        (cons last acc)
+        (apply
+          (lambda (key value)
+            (for-each
+              (lambda (i)
+                (hash-table-update! graph i (lambda (_) (delete value _))))
+              keys)
+            (loop key (cons value acc)))
+          (single graph))))))
 
 (define (solve input)
-  (list->string (path (graph input))))
+  (list->string (path input)))
 
 (print (solve (import-input)))
