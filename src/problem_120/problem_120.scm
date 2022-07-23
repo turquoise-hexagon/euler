24c9a2410ec04d72646d1597216206ada51d9a0b
turquoise-hexagon <camille@lzr.pw>
2022-07-23 02:34:46 +0100

    update structure of the repo

--- src/problem_120/problem_120.scm
+++
@@ -1,7 +0,0 @@
-(define (solve n)
-  (let solve/h ((i 3) (acc 0))
-    (if (> i n) acc
-        (let ((acc (+ acc (* 2 i (quotient (- i 1) 2)))))
-          (solve/h (+ i 1) acc)))))
-
-(print (solve 1000))
