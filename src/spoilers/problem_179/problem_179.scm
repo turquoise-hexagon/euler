0142c31ecb34c1e990e6b82b5b24ffa2e904fd20
turquoise-hexagon <camille@lzr.pw>
2022-07-26 17:16:25 +0100

    update problem_179

--- src/spoilers/problem_179/problem_179.scm
+++ src/spoilers/problem_179/problem_179.scm
@@ -1,16 +1,17 @@
 (define (generate n)
-  (let ((array (make-vector (+ n 1) 0)))
+  (let ((acc (make-vector (+ n 1) 0)))
     (do ((i 1 (+ i 1))) ((> i n))
       (do ((j i (+ j i))) ((> j n))
-        (vector-set! array j (+ 1 (vector-ref array j)))))
-    (vector->list array)))
+        (vector-set! acc j (+ (vector-ref acc j) 1))))
+    (vector->list acc)))
 
 (define (solve n)
-  (let solve/h ((lst (generate n)) (acc 0))
-    (if (null? (cdr lst)) acc
-        (let ((a (car lst)) (b (cadr lst)))
-          (solve/h (cdr lst) (if (= a b)
-                                 (+ acc 1)
-                                 acc))))))
+  (let loop ((lst (generate n)) (acc 0))
+    (if (null? (cdr lst))
+      acc
+      (loop (cdr lst)
+        (if (= (car lst) (cadr lst))
+          (+ acc 1)
+          acc)))))
 
 (print (solve 10000000))
