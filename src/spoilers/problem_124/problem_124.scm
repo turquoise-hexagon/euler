ed32fa3ca4baaa55346fa9b3f09a49cee24ae9e4
turquoise-hexagon <camille@lzr.pw>
2022-07-26 01:36:02 +0100

    update problem_124

--- src/spoilers/problem_124/problem_124.scm
+++ src/spoilers/problem_124/problem_124.scm
@@ -1,18 +1,20 @@
-(import (chicken sort)
-        (euler)
-        (srfi 1))
+(import
+  (chicken sort)
+  (euler)
+  (srfi 1))
 
 (define (rad n)
   (apply * (delete-duplicates (factorize n) =)))
 
 (define (solve a b)
-  (car (list-ref (sort
-                   (map
-                     (lambda (i)
-                       (cons i (rad i)))
-                     (iota a 1))
-                   (lambda (a b)
-                     (< (cdr a) (cdr b))))
-                 (- b 1))))
+  (let ((_ (sort
+             (map
+               (lambda (i)
+                 (cons i (rad i)))
+               (range 1 a))
+             (lambda (a b)
+               (< (cdr a)
+                  (cdr b))))))
+    (car (list-ref _ (- b 1)))))
 
 (print (solve 100000 10000))
