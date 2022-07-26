5b8feae024207a1843492614aaa03800696f2e61
turquoise-hexagon <camille@lzr.pw>
2022-07-26 01:38:54 +0100

    update problem_129

--- src/spoilers/problem_129/problem_129.scm
+++ src/spoilers/problem_129/problem_129.scm
@@ -1,15 +1,15 @@
 (define (a n)
   (if (= (gcd n 10) 1)
-      (let a/h ((i 1) (acc 1))
-        (if (= i 0)
-            acc
-            (a/h (modulo (+ (* i 10) 1) n) (+ acc 1))))
-      0))
+    (let loop ((i 1) (acc 1))
+      (if (= i 0)
+        acc
+        (loop (modulo (+ (* i 10) 1) n) (+ acc 1))))
+    0))
 
-(define (main n)
-  (let main/h ((i (+ n 1)))
+(define (solve n)
+  (let loop ((i (+ n 1)))
     (if (> (a i) n)
-        i
-        (main/h (+ i 2)))))
+      i
+      (loop (+ i 2)))))
 
-(print (main 1000000))
+(print (solve 1000000))
