53e383191c129f6827b36ae159b865c8b564f7da
turquoise-hexagon <camille@lzr.pw>
2022-08-09 10:37:04 +0100

    update problem_072

--- src/problem_072/problem_072.scm
+++ src/problem_072/problem_072.scm
@@ -1,31 +1,24 @@
 (import
   (euler))
 
-(define (factors n)
-  (let ((acc (make-vector (+ n 1) '())))
+(define (make-phi n)
+  (let ((factors (make-vector (+ n 1) '())))
     (for-each
       (lambda (p)
         (do ((i p (+ i p))) ((> i n))
-          (let ((_ (vector-ref acc i)))
+          (let ((_ (vector-ref factors i)))
             (unless (member p _)
-              (vector-set! acc i (cons p _))))))
+              (vector-set! factors i (cons p _))))))
       (primes n))
-    acc))
-
-(define (phi factors n)
-  (apply *
-    (cons n
-      (map
-        (lambda (_)
-          (- 1 (/ 1 _)))
-        (vector-ref factors n)))))
+    (lambda (n)
+      (foldl * n
+        (map
+          (lambda (_)
+            (- 1 (/ 1 _)))
+          (vector-ref factors n))))))
 
 (define (solve n)
-  (let ((factors (factors n)))
-    (foldl + 0
-      (map
-        (lambda (_)
-          (phi factors _))
-        (range 2 n)))))
+  (let ((phi (make-phi n)))
+    (foldl + 0 (map phi (range 2 n)))))
 
 (print (solve 1000000))
