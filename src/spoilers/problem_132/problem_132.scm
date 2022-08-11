207a258b199d3f5d9f26ae64089efaba74ecd2cd
turquoise-hexagon <camille@lzr.pw>
2022-08-11 16:04:15 +0100

    add problem_132

---
+++ src/spoilers/problem_132/problem_132.scm
@@ -0,0 +1,14 @@
+(import
+  (euler))
+
+(define (solve k n)
+  (let loop ((i 1) (cnt 0) (acc 0))
+    (if (= cnt n)
+      acc
+      (if (prime? i)
+        (if (= (expt-mod 10 k (* i 9)) 1)
+          (loop (+ i 1) (+ cnt 1) (+ acc i))
+          (loop (+ i 1) cnt acc))
+        (loop (+ i 1) cnt acc)))))
+
+(print (solve (expt 10 9) 40))
