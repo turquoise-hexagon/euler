14c9840757cada6599d25b30aab4d0083e58f1cc
turquoise-hexagon <camille@lzr.pw>
2022-08-27 00:48:47 +0100

    add problem_092

---
+++ src/problem_092/problem_092.scm
@@ -0,0 +1,33 @@
+(import
+  (srfi 69))
+
+(define (f n)
+  (let loop ((i n) (acc 0))
+    (if (= i 0)
+      acc
+      (let ((q (quotient i 10)) (m (modulo i 10)))
+        (loop q (+ acc (* m m)))))))
+
+(define cache
+  (alist->hash-table
+    '((1  . #f)
+      (89 . #t))))
+
+(define (chain n)
+  (let loop ((i n))
+    (if (hash-table-exists? cache i)
+      (let ((_ (hash-table-ref cache i)))
+        (hash-table-set! cache n _)
+        _)
+      (loop (f i)))))
+
+(define (solve n)
+  (let loop ((i 1) (acc 0))
+    (if (> i n)
+      acc
+      (loop (+ i 1)
+        (if (chain i)
+          (+ acc 1)
+          acc)))))
+
+(print (solve 10000000))
