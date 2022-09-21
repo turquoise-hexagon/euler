1e77c0bdf7e3355947be4d4a17c6fa144f31816c
turquoise-hexagon <camille@lzr.pw>
2022-09-21 08:28:50 +0100

    add problem_808

---
+++ src/spoilers/problem_808/problem_808.scm
@@ -0,0 +1,25 @@
+(import
+  (euler))
+
+(define (reverse-number n)
+  (let loop ((n n) (acc 0))
+    (if (= n 0)
+      acc
+      (loop (quotient n 10)
+        (+ (* acc 10) (modulo n 10))))))
+
+(define (solve n)
+  (let loop ((i 1) (acc 0) (cnt 0))
+    (if (= cnt n)
+      acc
+      (let* ((s (* i i)) (r (reverse-number s)))
+        (if (= s r)
+          (loop (+ i 2) acc cnt)
+          (let ((_ (sqrt r)))
+            (if (and (integer? _)
+                     (prime? _)
+                     (prime? i))
+              (loop (+ i 2) (+ acc s) (+ cnt 1))
+              (loop (+ i 2) acc cnt))))))))
+   
+(print (solve 50))
