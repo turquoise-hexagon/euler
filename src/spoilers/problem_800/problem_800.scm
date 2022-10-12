6174d7a8385aa192650b7ada5fc3b1c8bff69c62
turquoise-hexagon <camille@lzr.pw>
2022-10-12 13:23:32 +0100

    add problem_800

---
+++ src/spoilers/problem_800/problem_800.scm
@@ -0,0 +1,32 @@
+(import
+  (euler))
+
+(define (find-limit target)
+  (let ((_ (log 2)))
+    (let loop ((i 1))
+      (if (> (* i _) target)
+        i
+        (loop (+ i 1))))))
+
+(define (make-search n)
+  (let* ((target (* n (log n))) (primes (list->vector (primes (find-limit target)))))
+    (lambda (i)
+      (let loop ((a i) (b (- (vector-length primes) 1)))
+        (if (< a b)
+          (let* ((m (quotient (+ a b) 2))
+                 (p (vector-ref primes i))
+                 (q (vector-ref primes m)))
+            (if (< target (+ (* p (log q)) (* q (log p))))
+              (loop a m)
+              (loop (+ m 1) b)))
+          (- a 1))))))
+
+(define (solve n)
+  (let ((search (make-search n)))
+    (let loop ((i 0) (acc 0))
+      (let ((_ (search i)))
+        (if (> _ i)
+          (loop (+ i 1) (+ acc (- _ i)))
+          acc)))))
+
+(print (solve 800800))
