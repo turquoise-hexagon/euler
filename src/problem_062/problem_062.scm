5ae41b69ee0427753fd1956cfe7d8845f42c2cf6
turquoise-hexagon <camille@lzr.pw>
2022-10-20 00:39:53 +0100

    update problem_062

--- src/problem_062/problem_062.scm
+++ src/problem_062/problem_062.scm
@@ -3,18 +3,19 @@
   (euler)
   (srfi 69))
 
-(define (key n)
+(define (id n)
   (sort (number->list n) <))
 
 (define (solve n)
-  (let ((acc (make-hash-table)))
+  (let ((cache (make-hash-table)))
     (let loop ((i 1))
-      (let* ((val (* i i i)) (key (key val)))
-        (let ((tmp (cons val (hash-table-ref/default acc key '()))))
-          (if (= (length tmp) n)
-            (apply min tmp)
-            (begin
-              (hash-table-set! acc key tmp)
-              (loop (+ i 1)))))))))
+      (let* ((value (expt i 3)) (id (id value))
+             (acc (hash-table-update!/default cache id
+                    (lambda (_)
+                      (cons value _))
+                    '())))
+        (if (= (length acc) n)
+          (apply min acc)
+          (loop (+ i 1)))))))
 
 (print (solve 5))
