927a1d07f0d7d9bca459707288ca2ee1574915e6
turquoise-hexagon <camille@lzr.pw>
2022-10-24 21:12:25 +0100

    update problem_052

--- src/problem_052/problem_052.scm
+++ src/problem_052/problem_052.scm
@@ -1,22 +1,28 @@
-(import
-  (chicken sort)
-  (euler)
-  (srfi 1))
-
-(define (make-valid? n)
-  (let ((muls (range 1 n)))
-    (lambda (i)
-      (let ((tmp (map
-                   (lambda (mul)
-                     (sort (number->list (* i mul)) <))
-                   muls)))
-        (every equal? tmp (cdr tmp))))))
+(define (anagram? a b)
+  (let ((acc (make-vector 10 0)))
+    (define (_anagram? n increment)
+      (let loop ((n n))
+        (unless (= n 0)
+          (let ((_ (modulo n 10)))
+            (vector-set! acc _
+              (+ (vector-ref acc _) increment)))
+          (loop (quotient n 10)))))
+    (_anagram? a +1)
+    (_anagram? b -1)
+    (let loop ((i 0))
+      (if (> i 9)
+        #t
+        (if (= (vector-ref acc i) 0)
+          (loop (+ i 1))
+          #f)))))
 
 (define (solve n)
-  (let ((valid? (make-valid? n)))
-    (let loop ((i 1))
-      (if (valid? i)
+  (let loop ((i 1))
+    (let subloop ((m 2))
+      (if (> m n)
         i
-        (loop (+ i 1))))))
+        (if (anagram? i (* i m))
+          (subloop (+ m 1))
+          (loop (+ i 1)))))))
 
 (print (solve 6))
