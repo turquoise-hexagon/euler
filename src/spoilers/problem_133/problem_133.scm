08d3cdd2d3f74bab51aea10f325f3d99e7921bcf
turquoise-hexagon <camille@lzr.pw>
2022-10-25 17:55:53 +0100

    update problem_133

--- src/spoilers/problem_133/problem_133.scm
+++ src/spoilers/problem_133/problem_133.scm
@@ -3,11 +3,10 @@
   (srfi 1))
 
 (define (solve n)
-  (let ((_ (expt 10 16)))
-    (apply +
-      (filter
-        (lambda (p)
-          (not (= (expt-mod 10 _ (* 9 p)) 1)))
-        (primes n)))))
+  (apply +
+    (filter
+      (lambda (p)
+        (not (= (expt-mod 10 #e1e16 (* 9 p)) 1)))
+      (primes n))))
 
 (print (solve 100000))
