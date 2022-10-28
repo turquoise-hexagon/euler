c69d17a88271400ed07f11c65509d67a20ba798f
turquoise-hexagon <camille@lzr.pw>
2022-10-28 20:39:20 +0100

    add problem_206

---
+++ src/spoilers/problem_206/problem_206.scm
@@ -0,0 +1,33 @@
+(import
+  (euler)
+  (srfi 1))
+
+(define pattern
+  '(1 _ 2 _ 3 _ 4 _ 5 _ 6 _ 7 _ 8 _ 9 _ 0))
+
+(define (generate-bound)
+  (list->number
+    (map
+      (lambda (i)
+        (if (number? i) i 9))
+      pattern)))
+
+(define (generate-fixed-bound)
+  (let ((_ (inexact->exact (round (sqrt (generate-bound))))))
+    (- _ (modulo _ 10))))
+
+(define (valid? n)
+  (every
+    (lambda (digit match)
+      (if (number? match)
+        (= digit match)
+        #t))
+    (number->list n) pattern))
+
+(define (solve)
+  (let loop ((i (generate-fixed-bound)))
+    (if (valid? (* i i))
+      i
+      (loop (- i 10)))))
+
+(print (solve))
