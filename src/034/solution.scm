a899c28e0132504f0b149556fc7e86c86225f83e
turquoise-hexagon <camille@lzr.pw>
2023-01-20 21:26:50 +0000

    update 034

--- src/034/solution.scm
+++ src/034/solution.scm
@@ -1,30 +1,30 @@
 (import
-  (euler)
-  (srfi 1))
+  (chicken fixnum)
+  (euler))
 
-(define factorials
-  (list->vector
-    (map factorial (range 0 9))))
+(define factorials (list->vector (map factorial (range 0 9))))
 
-(define upper-bound
-  (let ((val (factorial 9)))
-    (let loop ((i 1))
-      (let ((tmp (expt 10 i)))
-        (if (> tmp (* i val))
-          tmp
-          (loop (+ i 1)))))))
+(define limit
+  (let ((_ (vector-ref factorials 9)))
+    (let loop ((a 1) (b 1))
+      (if (fx> a b)
+        a
+        (loop (fx* a 10) (fx+ b _))))))
 
 (define (valid? n)
-  (=
-    (apply +
-      (map
-        (lambda (i)
-          (vector-ref factorials i))
-        (number->list n)))
-    n))
+  (let loop ((i n) (acc 0))
+    (if (fx= i 0)
+      (fx= acc n)
+      (loop (fx/ i 10) (fx+ acc (vector-ref factorials (fxmod i 10)))))))
 
 (define (solve)
-  (apply + (filter valid? (range 3 upper-bound))))
+  (let loop ((i 3) (acc 0))
+    (if (fx> i limit)
+      acc
+      (loop (fx+ i 1)
+        (if (valid? i)
+          (fx+ acc i)
+          acc)))))
 
 (let ((_ (solve)))
   (print _) (assert (= _ 40730)))
