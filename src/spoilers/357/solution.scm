621834ce6ab983dd3106da4da5ace8b9e114caa8
turquoise-hexagon <camille@lzr.pw>
2023-01-21 02:19:53 +0000

    update 357

--- src/spoilers/357/solution.scm
+++ src/spoilers/357/solution.scm
@@ -1,31 +1,37 @@
 (import
+  (chicken fixnum)
   (euler)
   (srfi 69))
 
+(define (make-prime? n)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (p)
+        (hash-table-set! acc p #t))
+      (primes (fx+ n 1)))
+    (define (prime? n)
+      (hash-table-exists? acc n))
+    prime?))
+
 (define (solve n)
-  (define _cache
-    (let ((acc (make-hash-table)))
-      (for-each
-        (lambda (i)
-          (hash-table-set! acc i #t))
-        (primes (+ n 1)))
-      acc))
-
-  (define (_prime? n)
-    (hash-table-exists? _cache n))
-
-  (let ((sieve (make-vector (+ n 1) #t)))
-    (do ((i 1 (+ i 1))) ((> (* i i) n))
-      (when (vector-ref sieve i)
-        (do ((j (* i i) (+ j i))) ((> j n))
-          (when (vector-ref sieve j)
-            (unless (_prime? (+ i (quotient j i)))
-              (vector-set! sieve j #f))))))
-    (do ((i 1 (+ i 1))
-         (a 0 (if (vector-ref sieve i)
-                (+ a i)
-                a)))
-      ((> i n) a))))
+  (let ((prime? (make-prime? n)))
+    (let ((cache (make-vector (fx+ n 1) #t)))
+      (let loop ((a 1) (acc (fx/ (fx* n (fx+ n 1)) 2)))
+        (if (fx> (fx* a a) n)
+          acc
+          (if (vector-ref cache a)
+            (let subloop ((b (fx* a a)) (acc acc))
+              (if (fx> b n)
+                (loop (fx+ a 1) acc)
+                (subloop (fx+ b a)
+                  (if (vector-ref cache b)
+                    (if (prime? (fx+ a (fx/ b a)))
+                      acc
+                      (begin
+                        (vector-set! cache b #f)
+                        (fx- acc b)))
+                    acc))))
+            (loop (fx+ a 1) acc)))))))
 
 (let ((_ (solve #e1e8)))
   (print _) (assert (= _ 1739023853137)))
