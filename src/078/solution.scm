31e15b41715cc8b65a8dad5dc57bf597cd79ffe0
turquoise-hexagon <camille@lzr.pw>
2023-01-24 22:31:44 +0000

    update 078

--- src/078/solution.scm
+++ src/078/solution.scm
@@ -1,43 +1,41 @@
 (import
+  (euler)
   (srfi 69))
 
-(define cache
-  (make-hash-table))
-
-(define (pentagonal/h n)
-  (if (odd? n)
-    (/ (+ n 1) 2)
-    (+ n 1)))
-
-(define (pentagonal n)
-  (if (> n 1)
-    (+ (pentagonal   (- n 1))
-       (pentagonal/h (- n 1)))
-    1))
-
-(define (sign n)
-  (if (> (modulo (- n 1) 4) 1)
-    -1
-    +1))
-
-(define (partition n)
-  (if (hash-table-exists? cache n)
-    (hash-table-ref cache n)
-    (if (< n 2) 1
-      (let loop ((i 1) (acc 0))
-        (let ((p (pentagonal i)))
-          (if (> p n)
+(define (make-partition)
+  (let ((cache (make-hash-table)))
+    (hash-table-set! cache 0 1)
+    (hash-table-set! cache 1 1)
+    (define (partition n)
+      (if (hash-table-exists? cache n)
+        (hash-table-ref cache n)
+        (let loop ((i 1) (acc 0))
+          (if (> i n)
             (begin
               (hash-table-set! cache n acc)
               acc)
-            (loop (+ i 1)
-              (+ acc (* (sign i) (partition (- n p)))))))))))
+            (let*
+              ((a (- n (quotient (* i (- (* 3 i) 1)) 2)))
+               (b (- n (quotient (* i (+ (* 3 i) 1)) 2)))
+               (acc (+ acc (* (if (even? i)
+                                -1
+                                +1)
+                              (+ (partition a)
+                                 (partition b))))))
+              (if (and (< a 0)
+                       (< b 0))
+                (begin
+                  (hash-table-set! cache n acc)
+                  acc)
+                (loop (+ i 1) acc)))))))
+    partition))
 
 (define (solve n)
-  (let loop ((i 1))
-    (if (= (modulo (partition i) n) 0)
-      i
-      (loop (+ i 1)))))
+  (let ((partition (make-partition)))
+    (let loop ((i 1))
+      (if (= (modulo (partition i) n) 0)
+        i
+        (loop (+ i 1))))))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 55374)))
