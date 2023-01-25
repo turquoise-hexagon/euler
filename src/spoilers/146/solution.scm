a641dbdcfb2e68046742342f996be75a246bb063
turquoise-hexagon <camille@lzr.pw>
2023-01-25 15:36:04 +0000

    update 146

--- src/spoilers/146/solution.scm
+++ src/spoilers/146/solution.scm
@@ -1,38 +1,39 @@
 (import
+  (chicken fixnum)
   (euler))
 
 (define (valid? n)
-  (if (and (case (modulo n 3)
+  (if (and (case (fxmod n 3)
              ((1 2) #t)
              (else #f))
-           (case (modulo n 7)
+           (case (fxmod n 7)
              ((3 4) #t)
              (else #f))
-           (case (modulo n 13)
+           (case (fxmod n 13)
              ((1 3 4 9 10 12) #t)
              (else #f)))
-    (let ((_ (* n n)))
+    (let ((_ (fx* n n)))
       (let loop ((i 1))
-        (if (> i 27)
+        (if (fx> i 27)
           #t
           (case i
             ((1 3 7 9 13 27)
-             (if (prime? (+ _ i))
-               (loop (+ i 1))
+             (if (prime? (fx+ _ i))
+               (loop (fx+ i 1))
                #f))
             (else
-              (if (prime? (+ _ i))
+              (if (prime? (fx+ _ i))
                 #f
-                (loop (+ i 1))))))))
+                (loop (fx+ i 1))))))))
     #f))
 
 (define (solve n)
   (let loop ((i 0) (acc 0))
-    (if (> i n)
+    (if (fx> i n)
       acc
-      (loop (+ i 10)
+      (loop (fx+ i 10)
         (if (valid? i)
-          (+ acc i)
+          (fx+ acc i)
           acc)))))
 
 (let ((_ (solve #e15e7)))
