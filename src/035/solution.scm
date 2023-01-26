346c4bc2e0cfe0afff41df9b040af77cdae91a74
turquoise-hexagon <camille@lzr.pw>
2023-01-26 00:52:50 +0000

    update 035

--- src/035/solution.scm
+++ src/035/solution.scm
@@ -1,19 +1,39 @@
 (import
+  (chicken fixnum)
   (euler)
-  (srfi 1))
+  (srfi 69))
 
-(define (rotations n)
-  (let* ((tmp (number->list n)) (len (length tmp)))
-    (let loop ((i 0) (lst (apply circular-list tmp)) (acc '()))
-      (if (= i len)
-        (map list->number acc)
-        (loop (+ i 1) (cdr lst) (cons (take lst len) acc))))))
+(define (order n)
+  (let loop ((i 1))
+    (let ((_ (fx* i 10)))
+      (if (fx> _ n)
+        i
+        (loop _)))))
 
-(define (valid? n)
-  (every prime? (rotations n)))
+(define (make-circular-prime? primes)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (_)
+        (hash-table-set! acc _ #t))
+      primes)
+    (define (circular-prime? n)
+      (let ((_ (order n)))
+        (let loop ((i n) (n n))
+          (if (fx= i 0)
+            #t
+            (if (hash-table-exists? acc n)
+              (loop (fx/ i 10) (fx+ (fx* (fxmod n _) 10) (fx/ n _)))
+              #f)))))
+    circular-prime?))
 
 (define (solve n)
-  (count valid? (range 1 n)))
+  (let* ((primes (primes n)) (circular-prime? (make-circular-prime? primes)))
+    (foldl
+      (lambda (acc i)
+        (if (circular-prime? i)
+          (fx+ acc 1)
+          acc))
+      0 primes)))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 55)))
