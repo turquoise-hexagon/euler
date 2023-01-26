941278aac1b45ee971546aebb321220a4755fed6
turquoise-hexagon <camille@lzr.pw>
2023-01-26 04:57:58 +0000

    update 074

--- src/074/solution.scm
+++ src/074/solution.scm
@@ -1,35 +1,41 @@
 (import
+  (chicken fixnum)
   (euler)
   (srfi 69))
 
-(define factorials
-  (list->vector (map factorial (range 0 9))))
+(define factorials (list->vector (map factorial (range 0 9))))
 
-(define (sum-factorials-digits n)
-  (let loop ((i n) (acc 0))
-    (if (= i 0)
+(define (sum-factorial-digits n)
+  (let loop ((n n) (acc 0))
+    (if (fx= n 0)
       acc
-      (let ((q (quotient i 10)) (m (modulo i 10)))
-        (loop q (+ acc (vector-ref factorials m)))))))
+      (loop (fx/ n 10) (fx+ acc (vector-ref factorials (fxmod n 10)))))))
 
-(define (chain n)
-  (let ((mem (make-hash-table)))
-    (let loop ((i n) (acc 0))
-      (if (hash-table-exists? mem i)
+(define (make-chain)
+  (let ((cache (make-hash-table)))
+    (define (chain n)
+      (let ((mem (make-hash-table)))
+        (let loop ((n n))
+          (if (hash-table-exists? cache n)
+            (hash-table-ref cache n)
+            (if (hash-table-exists? mem n)
+              0
+              (begin
+                (hash-table-set! mem n #t)
+                (let ((_ (fx+ (loop (sum-factorial-digits n)) 1)))
+                  (hash-table-set! cache n _)
+                  _)))))))
+    chain))
+
+(define (solve n)
+  (let ((chain (make-chain)))
+    (let loop ((i 0) (acc 0))
+      (if (fx> i n)
         acc
-        (begin
-          (hash-table-set! mem i #t)
-          (loop (sum-factorials-digits i)
-            (+ acc 1)))))))
+        (loop (fx+ i 1)
+          (if (fx= (chain i) 60)
+            (fx+ acc 1)
+            acc))))))
 
-(define (solve lim cnt)
-  (let loop ((i 1) (acc 0))
-    (if (> i lim)
-      acc
-      (loop (+ i 1)
-        (if (= (chain i) cnt)
-          (+ acc 1)
-          acc)))))
-
-(let ((_ (solve #e1e6 60)))
+(let ((_ (solve #e1e6)))
   (print _) (assert (= _ 402)))
