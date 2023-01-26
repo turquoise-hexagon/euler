9e546e4a55bc140626a24696d7d48e4095feb2a3
turquoise-hexagon <camille@lzr.pw>
2023-01-26 05:57:39 +0000

    update 037

--- src/037/solution.scm
+++ src/037/solution.scm
@@ -1,6 +1,7 @@
 (import
   (chicken fixnum)
-  (euler))
+  (euler)
+  (srfi 69))
 
 (define (order n)
   (let loop ((i 1))
@@ -9,22 +10,31 @@
         i
         (loop _)))))
 
-(define (valid? n)
-  (let loop ((a n) (b n) (d (order n)))
-    (if (fx= a 0)
-      #t
-      (if (and (prime? a)
-               (prime? b))
-        (loop (fx/ a 10) (fxmod b d) (fx/ d 10))
-        #f))))
+(define (make-valid? primes)
+  (let ((acc (make-hash-table)))
+    (for-each
+      (lambda (_)
+        (hash-table-set! acc _ #t))
+      primes)
+    (define (valid? n)
+      (let loop ((a n) (b n) (d (order n)))
+        (if (fx= a 0)
+          #t
+          (if (and (hash-table-exists? acc a)
+                   (hash-table-exists? acc b))
+            (loop (fx/ a 10) (fxmod b d) (fx/ d 10))
+            #f))))
+    valid?))
 
 (define (solve)
-  (let loop ((i 9) (cnt 0) (acc 0))
-    (if (fx= cnt 11)
-      acc
-      (if (valid? i)
-        (loop (fx+ i 2) (fx+ cnt 1) (fx+ acc i))
-        (loop (fx+ i 2) cnt acc)))))
+  (let* ((primes (primes #e1e6)) (valid? (make-valid? primes)))
+    (let loop ((lst (cddddr primes)) (cnt 0) (acc 0))
+      (if (fx= cnt 11)
+        acc
+        (let ((i (car lst)))
+          (if (valid? i)
+            (loop (cdr lst) (fx+ cnt 1) (fx+ acc i))
+            (loop (cdr lst) cnt acc)))))))
 
 (let ((_ (solve)))
   (print _) (assert (= _ 748317)))
