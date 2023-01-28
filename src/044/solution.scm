838e9417844413bd257dc415804039ec5aeee7df
turquoise-hexagon <camille@lzr.pw>
2023-01-28 20:44:42 +0000

    update 044

--- src/044/solution.scm
+++ src/044/solution.scm
@@ -1,25 +1,42 @@
 (import
-  (euler))
+  (chicken fixnum))
 
-(define (pentagonal n)
-  (/ (* n (- (* 3 n) 1)) 2))
+(define-constant limit #e1e7)
 
-(define (pentagonal? n)
-  (integer? (/ (+ (sqrt (+ (* 24 n) 1)) 1) 6)))
+(define-syntax pentagonal
+  (syntax-rules ()
+    ((_ n)
+     (fx/ (fx* n (fx- (fx* 3 n) 1)) 2))))
+
+(define (pentagonals limit)
+  (let loop ((i 1) (acc '()))
+    (let ((p (pentagonal i)))
+      (if (fx> p limit)
+        acc
+        (loop (fx+ i 1) (cons p acc))))))
+
+(define (make-pentagonal? limit)
+  (let ((acc (make-vector (fx+ limit 1) #f)))
+    (for-each
+      (lambda (p)
+        (vector-set! acc p #t))
+      (pentagonals limit))
+    (define (pentagonal? n)
+      (vector-ref acc n))
+    pentagonal?))
 
 (define (solve)
-  (call/cc
-    (lambda (_)
-      (let loop ((i 1))
-        (let ((a (pentagonal i)))
-          (for-each
-            (lambda (j)
-              (let ((b (pentagonal j)))
-                (when (and (pentagonal? (+ a b))
-                           (pentagonal? (- a b)))
-                  (_ (abs (- a b))))))
-            (range 1 i)))
-        (loop (+ i 1))))))
+  (let ((pentagonal? (make-pentagonal? (fx* limit 2))))
+    (let loop ((a (pentagonals limit)))
+      (let subloop ((b a))
+        (if (null? b)
+          (loop (cdr a))
+          (let ((s (fx+ (car a) (car b)))
+                (d (fx- (car a) (car b))))
+            (if (and (pentagonal? s)
+                     (pentagonal? d))
+              d
+              (subloop (cdr b)))))))))
 
 (let ((_ (solve)))
   (print _) (assert (= _ 5482660)))
