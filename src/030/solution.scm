82d8cd6599c7a3ea6f66adee29bfc255d241b8f9
turquoise-hexagon <camille@lzr.pw>
2023-01-28 21:30:41 +0000

    update 030

--- src/030/solution.scm
+++ src/030/solution.scm
@@ -1,37 +1,35 @@
 (import
-  (euler)
-  (srfi 1))
+  (chicken fixnum)
+  (euler))
 
-(define (powers e)
+(define (digits e)
   (list->vector
     (map
       (lambda (i)
         (expt i e))
       (range 0 9))))
 
-(define (upper-bound e)
-  (let ((val (expt 9 e)))
-    (let loop ((i 1))
-      (let ((tmp (expt 10 i)))
-        (if (> tmp (* i val))
-          tmp
-          (loop (+ i 1)))))))
+(define (bounds d)
+  (let loop ((a 0) (b 0))
+    (if (fx> a b)
+      a
+      (loop (fx+ (fx* a 10) 9) (fx+ b (vector-ref d 9))))))
 
-(define (valid? n powers)
-  (= (apply +
-       (map
-         (lambda (i)
-           (vector-ref powers i))
-         (number->list n)))
-     n))
+(define (valid? n d)
+  (let loop ((i n) (acc 0))
+    (if (fx= i 0)
+      (fx= n acc)
+      (loop (fx/ i 10) (fx+ acc (vector-ref d (fxmod i 10)))))))
 
 (define (solve e)
-  (let ((powers (powers e)))
-    (apply +
-      (filter
-        (lambda (n)
-          (valid? n powers))
-        (range 2 (upper-bound e))))))
+  (let* ((d (digits e)) (b (bounds d)))
+    (let loop ((i 2) (acc 0))
+      (if (fx> i b)
+        acc
+        (loop (fx+ i 1)
+          (if (valid? i d)
+            (fx+ acc i)
+            acc))))))
 
 (let ((_ (solve 5)))
   (print _) (assert (= _ 443839)))
