27f5da858528907a17d9fb452223693e23eb40c3
turquoise-hexagon <camille@lzr.pw>
2023-01-28 06:05:09 +0000

    update 012

--- src/012/solution.scm
+++ src/012/solution.scm
@@ -1,11 +1,18 @@
 (import
   (euler))
 
+(define (number-divisors n)
+  (apply *
+    (map
+      (lambda (_)
+        (+ (car _) 1))
+      (run-length (factorize n)))))
+
 (define (solve n)
-  (let loop ((a 1) (b 2))
-    (if (> (length (divisors a)) n)
-      a
-      (loop (+ a b) (+ b 1)))))
+  (let loop ((i 0) (acc 0))
+    (if (> (number-divisors acc) n)
+      acc
+      (loop (+ i 1) (+ acc i)))))
 
 (let ((_ (solve 500)))
   (print _) (assert (= _ 76576500)))
