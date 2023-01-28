a7f5304262fa8bbc4da228d745adb7bce9b8a8f4
turquoise-hexagon <camille@lzr.pw>
2023-01-28 06:08:09 +0000

    update 004

--- src/004/solution.scm
+++ src/004/solution.scm
@@ -1,17 +1,19 @@
 (import
-  (euler)
-  (srfi 1))
+  (euler))
 
 (define (solve n)
-  (apply max
-    (filter palindrome?
-      (map
-        (lambda (pair)
-          (apply * pair))
-        (combinations
-          (range (expt 10 (- n 1))
-                 (- (expt 10 n) 1))
-          2)))))
+  (let ((s (expt 10 (- n 1))) (e (- (expt 10 n) 1)))
+    (let loop ((i s) (acc '()))
+      (if (> i e)
+        (apply max acc)
+        (let subloop ((j i) (acc acc))
+          (if (> j e)
+            (loop (+ i 1) acc)
+            (subloop (+ j 1)
+              (let ((_ (* i j)))
+                (if (palindrome? _)
+                  (cons _ acc)
+                  acc)))))))))
 
 (let ((_ (solve 3)))
   (print _) (assert (= _ 906609)))
