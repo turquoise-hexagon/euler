2324a2718831a728daae0cb374f0bf71914472ba
turquoise-hexagon <camille@lzr.pw>
2023-01-28 17:13:31 +0000

    update 046

--- src/046/solution.scm
+++ src/046/solution.scm
@@ -1,23 +1,47 @@
 (import
-  (euler)
-  (srfi 69))
+  (chicken fixnum)
+  (euler))
 
-(define limit 5000)
+(define-constant limit #e1e4)
 
-(define (generate)
-  (let ((acc (make-hash-table)) (lst (range 0 limit)))
+(define (double-squares limit)
+  (let loop ((i 1) (acc '()))
+    (let ((_ (fx* (fx* i i) 2)))
+      (if (fx> _ limit)
+        acc
+        (loop (fx+ i 1) (cons _ acc))))))
+
+(define (make-prime? primes limit)
+  (let ((acc (make-vector (fx+ limit 1) #f)))
     (for-each
-      (lambda (p)
-        (for-each (lambda (_) (hash-table-set! acc (+ p (* 2 _ _)) #t)) lst))
-      (primes limit))
-    acc))
+      (lambda (i)
+        (vector-set! acc i #t))
+      primes)
+    (define (prime? n)
+      (vector-ref acc n))
+    prime?))
+
+(define (make-valid? limit)
+  (let* ((acc (make-vector (fx+ limit 1) #f)) (double-squares (double-squares limit)) (primes (primes limit)) (prime? (make-prime? primes limit)))
+    (for-each
+      (lambda (i)
+        (for-each
+          (lambda (j)
+            (let ((_ (fx+ i j)))
+              (unless (fx> _ limit)
+                (vector-set! acc _ #t))))
+          double-squares))
+      primes)
+    (define (valid? n)
+      (and (not (vector-ref acc n)) (not (prime? n))))
+    valid?))
 
 (define (solve)
-  (let ((table (generate)))
+  (let ((valid? (make-valid? limit)))
     (let loop ((i 3))
-      (if (hash-table-exists? table i)
-        (loop (+ i 2))
-        i))))
+      (if (valid? i)
+        i
+        (loop (fx+ i 2))))))
 
 (let ((_ (solve)))
   (print _) (assert (= _ 5777)))
