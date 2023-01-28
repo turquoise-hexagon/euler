7055cdc671bcc6efac2f6e8551b276cb5cf96e4b
turquoise-hexagon <camille@lzr.pw>
2023-01-28 05:56:09 +0000

    update 043

--- src/043/solution.scm
+++ src/043/solution.scm
@@ -2,11 +2,11 @@
   (euler)
   (srfi 1))
 
-(define divs
-  '(1 2 3 5 7 11 13 17))
+(define-constant divs
+  '(2 3 5 7 11 13 17))
 
 (define (valid? lst)
-  (let loop ((lst lst) (divs divs))
+  (let loop ((lst (cdr lst)) (divs divs))
     (if (null? divs)
       #t
       (let ((_ (list->number (take lst 3))))
