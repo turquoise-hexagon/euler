4c40455b3f78b4b6bb7409e8fe7323dda74b2a63
turquoise-hexagon <camille@lzr.pw>
2023-02-01 07:19:58 +0000

    update 036

--- src/036/solution.scm
+++ src/036/solution.scm
@@ -1,14 +1,21 @@
 (import
-  (euler)
-  (srfi 1))
+  (chicken fixnum))
+
+(define (palindrome? n #!optional (b 10))
+  (let loop ((i n) (acc 0))
+    (if (fx= i 0)
+      (fx= n acc)
+      (loop (fx/ i b) (fx+ (fx* acc b) (fxmod i b))))))
 
 (define (solve n)
-  (apply +
-    (filter
-      (lambda (i)
-        (and (palindrome? i 2)
-             (palindrome? i 10)))
-      (range 1 n))))
+  (let loop ((i 0) (acc 0))
+    (if (fx> i n)
+      acc
+      (loop (fx+ i 1)
+        (if (and (palindrome? i)
+                 (palindrome? i 2))
+          (fx+ acc i)
+          acc)))))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 872187)))
