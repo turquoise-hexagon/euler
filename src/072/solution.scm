f6b3c9e1cc15e307aeb9e6cd29b9da865623439c
turquoise-hexagon <camille@lzr.pw>
2023-02-03 11:26:12 +0000

    update 072

--- src/072/solution.scm
+++ src/072/solution.scm
@@ -1,14 +1,15 @@
 (import
+  (chicken fixnum)
   (euler))
 
 (define (make-phi n)
-  (let ((acc (make-vector (+ n 1) 1)))
-    (do ((i 0 (+ i 1))) ((> i n))
+  (let ((acc (make-vector (fx+ n 1) 1)))
+    (do ((i 0 (fx+ i 1))) ((fx> i n))
       (vector-set! acc i i))
     (for-each
       (lambda (p)
-        (do ((m p (+ m p))) ((> m n))
-          (vector-set! acc m (* (vector-ref acc m) (- 1 (/ 1 p))))))
+        (do ((m p (fx+ m p))) ((fx> m n))
+          (vector-set! acc m (fx- (vector-ref acc m) (fx/ (vector-ref acc m) p)))))
       (primes n))
     (define (phi n)
       (vector-ref acc n))
@@ -16,9 +17,9 @@
 
 (define (solve n)
   (let ((phi (make-phi n)))
-    (do ((i 2 (+ i 1))
-         (acc 0 (+ acc (phi i))))
-      ((> i n) acc))))
+    (do ((i 2 (fx+ i 1))
+         (acc 0 (fx+ acc (phi i))))
+      ((fx> i n) acc))))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 303963552391)))
