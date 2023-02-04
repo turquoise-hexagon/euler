59fcbba10c059d959f76a7dfbaca5893c619717c
turquoise-hexagon <camille@lzr.pw>
2023-02-04 06:26:12 +0000

    add 243

---
+++ src/spoilers/243/solution.scm
@@ -0,0 +1,30 @@
+(import
+  (euler)
+  (srfi 1))
+
+(define (phi n)
+  (foldl
+    (lambda (acc p)
+      (- acc (quotient acc p)))
+    n (delete-duplicates (factorize n))))
+
+(define (resilience n)
+  (/ (phi n) (- n 1)))
+
+(define (jump n)
+  (let loop ((lst (primes 100)) (acc 1))
+    (let ((tmp (* acc (car lst))))
+      (if (< (resilience tmp) n)
+        acc
+        (loop (cdr lst) tmp)))))
+
+(define (solve n)
+  (let ((prod (jump n)))
+    (let loop ((i 1))
+      (let ((tmp (* prod i)))
+        (if (< (resilience tmp) n)
+          tmp
+          (loop (+ i 1)))))))
+
+(let ((_ (solve 15499/94744)))
+  (print _) (assert (= _ 892371480)))
