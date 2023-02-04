43623e50e31091d2d06ecdd739166241cfe8b150
turquoise-hexagon <camille@lzr.pw>
2023-02-04 08:10:51 +0000

    update 052

--- src/052/solution.scm
+++ src/052/solution.scm
@@ -1,31 +1,39 @@
-(define-syntax _anagram?
-  (syntax-rules ()
-    ((_ acc n increment)
-     (let loop ((n n))
-       (unless (= n 0)
-         (let ((_ (modulo n 10)))
-           (vector-set! acc _ (+ (vector-ref acc _) increment)))
-         (loop (quotient n 10)))))))
+(import
+  (chicken fixnum))
 
-(define (anagram? a b)
+(define-syntax helper!
+  (syntax-rules ()
+    ((_ n acc increment)
+     (let loop ((n n))
+       (unless (fx= n 0)
+         (let ((_ (fxmod n 10)))
+           (vector-set! acc _ (fx+ (vector-ref acc _) increment)))
+         (loop (fx/ n 10)))))))
+
+(define (permutation? a b)
   (let ((acc (make-vector 10 0)))
-    (_anagram? acc a +1)
-    (_anagram? acc b -1)
+    (helper! a acc +1)
+    (helper! b acc -1)
     (let loop ((i 0))
-      (if (> i 9)
+      (if (fx= i 10)
         #t
-        (if (= (vector-ref acc i) 0)
-          (loop (+ i 1))
+        (if (fx= (vector-ref acc i) 0)
+          (loop (fx+ i 1))
           #f)))))
 
-(define (solve n)
+(define (valid? n l)
+  (let loop ((i 2) (s (fx+ n n)))
+    (if (fx> i l)
+      #t
+      (if (permutation? n s)
+        (loop (fx+ i 1) (fx+ s n))
+        #f))))
+
+(define (solve l)
   (let loop ((i 1))
-    (let subloop ((m 2))
-      (if (> m n)
-        i
-        (if (anagram? i (* i m))
-          (subloop (+ m 1))
-          (loop (+ i 1)))))))
+    (if (valid? i l)
+      i
+      (loop (fx+ i 1)))))
 
 (let ((_ (solve 6)))
   (print _) (assert (= _ 142857)))
