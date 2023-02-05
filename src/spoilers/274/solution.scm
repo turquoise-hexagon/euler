fa058f7c391e8c9fd79cd8c143b924fca763f3ef
turquoise-hexagon <camille@lzr.pw>
2023-02-05 06:04:22 +0000

    update 274

--- src/spoilers/274/solution.scm
+++ src/spoilers/274/solution.scm
@@ -1,13 +1,17 @@
 (import
   (euler))
 
+(define (delete-once l n)
+  (if (= (car l) n)
+    (cdr l)
+    (cons (car l) (delete-once (cdr l) n))))
+
 (define (solve n l)
-  (foldl
-    (lambda (acc p)
-      (if (= (gcd n p) 1)
-        (+ acc (modular-inverse n p))
-        acc))
-    0 (primes l)))
+  (foldl + 0
+    (map
+      (lambda (i)
+        (modular-inverse n i))
+      (foldl delete-once (primes l) (factorize n)))))
 
 (let ((_ (solve 10 #e1e7)))
   (print _) (assert (= _ 1601912348822)))
