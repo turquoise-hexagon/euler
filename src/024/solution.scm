c3d9d461e5f521c0599fb07f2fd147d7a467a9fe
turquoise-hexagon <camille@lzr.pw>
2023-02-06 23:04:36 +0000

    update 024

--- src/024/solution.scm
+++ src/024/solution.scm
@@ -1,8 +1,16 @@
 (import
   (euler))
 
+(define (nth-permutation l n)
+  (let ((i (length l)))
+    (let loop ((l l) (i i) (n (- n 1)))
+      (if (null? l)
+        '()
+        (let-values (((q r) (quotient&remainder n (factorial (- i 1)))))
+          (cons (list-ref l q) (loop (delete-at l q) (- i 1) r)))))))
+
 (define (solve n)
-  (list->number (list-ref (permutations (range 0 9)) (- n 1))))
+  (list->number (nth-permutation (range 0 9) n)))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 2783915460)))
