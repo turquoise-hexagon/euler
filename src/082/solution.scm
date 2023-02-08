b2a45f6a8f0542504b51b29bcf7827d61695c8ad
turquoise-hexagon <camille@lzr.pw>
2023-02-08 12:30:21 +0000

    update 082

--- src/082/solution.scm
+++ src/082/solution.scm
@@ -5,10 +5,17 @@
   (srfi 1)
   (srfi 69))
 
-(define offsets
-  '(( 1 0)
-    ( 0 1)
-    (-1 0)))
+(define-constant offsets
+  '((-1 0)
+    ( 1 0)
+    ( 0 1)))
+
+(define (import-input)
+  (list->array
+    (map
+      (lambda (str)
+        (map string->number (string-split str ",")))
+      (read-lines))))
 
 (define (neighbors array coord)
   (filter
@@ -19,53 +26,36 @@
         (map + coord offset))
       offsets)))
 
-(define (import-input)
-  (list->array
-    (map
-      (lambda (str)
-        (map string->number (string-split str ",")))
-      (read-lines))))
-
-(define (comp? a b)
+(define (compare? a b)
   (< (car a)
      (car b)))
 
-(define (helper! array table queue cost coord)
-  (foldl
-    (lambda (queue coord)
-      (let ((cost (+ cost (array-ref array coord))))
-        (if (< cost (hash-table-ref/default table coord +inf.0))
-          (begin
-            (hash-table-set! table coord cost)
-            (priority-queue-insert queue `(,cost ,coord)))
-          queue)))
-    (priority-queue-rest queue) (neighbors array coord)))
-
-(define (path array from)
+(define (find-path array fs ts)
   (let ((acc (make-hash-table)))
-    (let loop ((queue (list->priority-queue `((,(array-ref array from) ,from)) comp?)))
+    (let loop ((queue (list->priority-queue (map (lambda (i) (list (array-ref array i) i)) fs) compare?)))
       (if (priority-queue-empty? queue)
-        acc
-        (apply
-          (lambda (cost coord)
-            (loop (helper! array acc queue cost coord)))
-          (priority-queue-first queue))))))
+        (apply min (map (lambda (coord) (hash-table-ref acc coord)) ts))
+        (loop
+          (apply
+            (lambda (cost coord)
+              (foldl
+                (lambda (queue coord)
+                  (let ((cost (+ cost (array-ref array coord))))
+                    (if (> (hash-table-ref/default acc coord +inf.0) cost)
+                      (begin
+                        (hash-table-set! acc coord cost)
+                        (priority-queue-insert queue (list cost coord)))
+                      queue)))
+                (priority-queue-rest queue) (neighbors array coord)))
+            (priority-queue-first queue)))))))
 
 (define (solve input)
   (apply
     (lambda (h w)
-      (let ((r (product (range 0 (- h 1)) (list 0)))
-            (l (product (range 0 (- h 1)) (list (- w 1)))))
-        (apply min
-          (flatten
-            (map
-              (lambda (r)
-                (let ((table (path input r)))
-                  (map
-                    (lambda (l)
-                      (hash-table-ref table l))
-                    l)))
-              r)))))
+      (let ((a (- h 1)) (b (- w 1)))
+        (find-path input
+          (map (lambda (i) (list i 0)) (range 0 a))
+          (map (lambda (i) (list i b)) (range 0 a)))))
     (array-dimensions input)))
 
 (let ((_ (solve (import-input))))
