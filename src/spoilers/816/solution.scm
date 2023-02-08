f49343b6e18ffb4044f1440f0818044e57392e16
turquoise-hexagon <camille@lzr.pw>
2023-02-08 09:53:22 +0000

    update 816

--- src/spoilers/816/solution.scm
+++ src/spoilers/816/solution.scm
@@ -1,44 +1,43 @@
 (import
-  (chicken sort)
   (chicken flonum)
-  (chicken format))
+  (chicken format)
+  (chicken sort)
+  (euler))
 
 (define (make-generator)
   (set! i 290797)
-  (lambda ()
+  (define (generator)
     (let ((_ i))
       (set! i (modulo (* i i) 50515093))
-      _)))
+      _))
+  generator)
 
-(define (generate limit)
+(define (generate n)
   (let ((generator (make-generator)))
     (let loop ((i 1) (acc '()))
-      (if (> i limit)
+      (if (> i n)
         acc
-        (loop (+ i 1)
-          (let ((_ (list (generator) (generator))))
-            (cons _ acc)))))))
+        (loop (+ i 1) (cons (list (generator) (generator)) acc))))))
 
 (define (distance a b)
   (sqrt
     (apply +
       (map
-        (lambda (_)
-          (* _ _))
+        (lambda (i)
+          (* i i))
         (map - a b)))))
 
-(define (solve limit)
-  (let ((_
-          (map car
-            (sort
-              (map
-                (lambda (_)
-                  (cons _ (distance _ '(0 0))))
-                (generate limit))
-              (lambda (a b)
-                (< (cdr a)
-                   (cdr b)))))))
-    (foldl min +inf.0 (map distance _ (cdr _)))))
+(define (solve n)
+  (let ((l (map car
+             (sort
+               (map
+                 (lambda (i)
+                   (cons i (distance i '(0 0))))
+                 (generate n))
+               (lambda (a b)
+                 (< (cdr a)
+                    (cdr b)))))))
+    (foldl min +inf.0 (map distance l (cdr l)))))
 
 (define (output n)
   (flonum-print-precision 11) (format "~a" n))
