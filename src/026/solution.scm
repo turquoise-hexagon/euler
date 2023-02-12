cf3fb5e10b5008426f551594721cdd7d3c553040
turquoise-hexagon <camille@lzr.pw>
2023-02-12 16:45:45 +0000

    update 026

--- src/026/solution.scm
+++ src/026/solution.scm
@@ -1,8 +1,16 @@
 (import
-  (euler))
+  (euler)
+  (srfi 1))
 
 (define (solve n)
-  (extremum (primes n) (lambda (i) (discrete-log 10 1 i)) >))
+  (extremum
+    (filter
+      (lambda (i)
+        (= (gcd 10 i) 1))
+      (range 2 n))
+    (lambda (i)
+      (discrete-log 10 1 i))
+    >))
 
 (let ((_ (solve 1000)))
   (print _) (assert (= _ 983)))
