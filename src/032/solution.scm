644f32dddee2179ef1d625ffae28b55f978141e4
turquoise-hexagon <camille@lzr.pw>
2023-02-13 08:30:03 +0000

    update 032

--- src/032/solution.scm
+++ src/032/solution.scm
@@ -1,37 +1,30 @@
 (import
-  (chicken bitwise)
-  (euler)
+  (chicken fixnum)
   (srfi 69))
 
 (define (pandigital? n)
-  (if (= n 0)
+  (if (fx= n 0)
     #f
     (let loop ((n n) (c 0) (d 0))
-      (if (= n 0)
-        (= d (- (arithmetic-shift 1 c) 1))
-        (let ((t (bitwise-ior d (arithmetic-shift 1 (- (modulo n 10) 1)))))
-          (if (= d t)
+      (if (fx= n 0)
+        (fx= d (fx- (fxshl 1 c) 1))
+        (let ((t (fxior d (fxshl 1 (fx- (fxmod n 10) 1)))))
+          (if (fx= d t)
             #f
-            (loop (quotient n 10) (+ c 1) t)))))))
+            (loop (fx/ n 10) (fx+ c 1) t)))))))
 
 (define (concatenate a b)
   (let loop ((i b) (a a))
-    (if (= i 0)
-      (+ a b)
-      (loop (quotient i 10) (* a 10)))))
+    (if (fx= i 0)
+      (fx+ a b)
+      (loop (fx/ i 10) (fx* a 10)))))
 
 (define (helper! cache a/min a/max b/min b/max)
-  (let ((a (range a/min a/max))
-        (b (range b/min b/max)))
-    (for-each
-      (lambda (a)
-        (for-each
-          (lambda (b)
-            (let ((_ (* a b)))
-              (when (pandigital? (concatenate a (concatenate b _)))
-                (hash-table-set! cache _ #t))))
-          b))
-      a)))
+  (do ((a a/min (fx+ a 1))) ((fx> a a/max))
+    (do ((b b/min (fx+ b 1))) ((fx> b b/max))
+      (let* ((c (fx* a b)) (i (concatenate a (concatenate b c))))
+        (when (pandigital? i)
+          (hash-table-set! cache c #t))))))
 
 (define (solve)
   (let ((acc (make-hash-table)))
@@ -42,7 +35,7 @@
     |#
     (helper! acc 10 99 100 999)
     (helper! acc 1 9 1000 9999)
-    (apply + (hash-table-keys acc))))
+    (foldl fx+ 0 (hash-table-keys acc))))
 
 (let ((_ (solve)))
   (print _) (assert (= _ 45228)))
