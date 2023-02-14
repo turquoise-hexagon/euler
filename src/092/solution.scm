4e00496302014bf8555508bef1f638e8708557ca
turquoise-hexagon <camille@lzr.pw>
2023-02-14 15:42:04 +0000

    update 092

--- src/092/solution.scm
+++ src/092/solution.scm
@@ -10,28 +10,26 @@
         (let ((_ (fxmod n 10)))
           (fx+ acc (fx* _ _)))))))
 
-(define (make-chain)
+(define chain
   (let ((cache (make-hash-table)))
     ;; init cache
     (hash-table-set! cache  1 #f)
     (hash-table-set! cache 89 #t)
-    (define (chain n)
+    (lambda (n)
       (if (hash-table-exists? cache n)
         (hash-table-ref cache n)
         (let ((_ (chain (f n))))
           (hash-table-set! cache n _)
-          _)))
-    chain))
+          _)))))
 
 (define (solve n)
-  (let ((chain (make-chain)))
-    (let loop ((i 1) (acc 0))
-      (if (fx> i n)
-        acc
-        (loop (fx+ i 1)
-          (if (chain i)
-            (fx+ acc 1)
-            acc))))))
+  (let loop ((i 1) (acc 0))
+    (if (fx> i n)
+      acc
+      (loop (fx+ i 1)
+        (if (chain i)
+          (fx+ acc 1)
+          acc)))))
 
 (let ((_ (solve #e1e7)))
   (print _) (assert (= _ 8581146)))
