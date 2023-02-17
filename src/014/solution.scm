809c0c7f5b8a793c52c765e35c0c06bee0802c55
turquoise-hexagon <camille@lzr.pw>
2023-02-17 13:27:54 +0000

    update 014

--- src/014/solution.scm
+++ src/014/solution.scm
@@ -1,28 +1,31 @@
 (import
   (srfi 69))
 
+(define-syntax f
+  (syntax-rules ()
+    ((_ n)
+     (if (even? n)
+       (quotient n 2)
+       (+ (* n 3) 1)))))
+
 (define collatz
-  (let ((acc (make-hash-table)))
-    (hash-table-set! acc 1 1)
+  (let ((cache (make-hash-table)))
+    (hash-table-set! cache 1 1)
     (lambda (n)
-      (if (hash-table-exists? acc n)
-        (hash-table-ref acc n)
-        (let ((_ (+ (collatz
-                      (if (even? n)
-                        (quotient n 2)
-                        (+ (* 3 n) 1)))
-                    1)))
-          (hash-table-set! acc n _)
-          _)))))
+      (if (hash-table-exists? cache n)
+        (hash-table-ref cache n)
+        (let ((acc (+ (collatz (f n)) 1)))
+          (hash-table-set! cache n acc)
+          acc)))))
 
 (define (solve n)
-  (let loop ((i 1) (tmp 0) (acc 0))
+  (let loop ((i 1) (res 0) (acc 0))
     (if (> i n)
       acc
-      (let ((_ (collatz i)))
-        (if (> _ tmp)
-          (loop (+ i 1) _ i)
-          (loop (+ i 1) tmp acc))))))
+      (let ((tmp (collatz i)))
+        (if (> tmp res)
+          (loop (+ i 1) tmp i)
+          (loop (+ i 1) res acc))))))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 837799)))
