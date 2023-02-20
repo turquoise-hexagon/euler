b73d026462f4b00aa242347279aeb2c87bd4e425
turquoise-hexagon <camille@lzr.pw>
2023-02-20 22:47:20 +0000

    update 006

--- src/006/solution.scm
+++ src/006/solution.scm
@@ -1,9 +1,6 @@
-(import
-  (euler))
-
 (define (solve n)
-  (let ((lst (range 1 n)))
-    (- (expt (apply + lst) 2) (apply + (map (lambda (_) (expt _ 2)) lst)))))
+  (- (expt (quotient (* n (+ n 1)) 2) 2)
+     (quotient (* n (+ n 1) (+ (* 2 n) 1)) 6)))
 
 (let ((_ (solve 100)))
   (print _) (assert (= _ 25164150)))
