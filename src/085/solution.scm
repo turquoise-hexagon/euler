27ae19566b1384bebb912974c0f11ad030209401
turquoise-hexagon <camille@lzr.pw>
2023-02-21 22:03:08 +0000

    add 085

---
+++ src/085/solution.scm
@@ -0,0 +1,31 @@
+(import
+  (chicken fixnum))
+
+(define-syntax f
+  (syntax-rules ()
+    ((_ a b)
+     (fx/ (fx* (fx* a (fx+ a 1))
+               (fx* b (fx+ b 1)))
+          4))))
+
+(define-syntax fxabs
+  (syntax-rules ()
+    ((_ n)
+     (if (fx< n 0)
+       (fxneg n)
+       n))))
+
+(define (solve n)
+  (let loop ((a 1) (tmp n) (acc 0))
+    (if (fx> (fx* a a) n)
+      acc
+      (let subloop ((b 1) (tmp tmp) (acc acc))
+        (if (fx= a b)
+          (loop (fx+ a 1) tmp acc)
+          (let ((_ (fxabs (fx- n (f a b)))))
+            (if (fx< _ tmp)
+              (subloop (fx+ b 1) _ (fx* a b))
+              (subloop (fx+ b 1) tmp acc))))))))
+
+(let ((_ (solve #e2e6)))
+  (print _) (assert (= _ 2772)))
