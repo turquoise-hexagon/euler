33a51e5a34f8f416130ab252db7a8da30a02a634
turquoise-hexagon <camille@lzr.pw>
2023-02-24 22:48:32 +0000

    update 346

--- src/spoilers/346/solution.scm
+++ src/spoilers/346/solution.scm
@@ -1,24 +1,20 @@
 (import
   (srfi 69))
 
-(define-syntax t
+(define-syntax f
   (syntax-rules ()
-    ((_ n b)
-     (quotient (- (expt b n) 1) (- b 1)))))
+    ((_ a b)
+     (quotient (- (expt b a) 1) (- b 1)))))
 
-(define (solve limit)
+(define (solve n)
   (let ((acc (make-hash-table)))
-    (hash-table-set! acc 1 1)
-    (let loop ((n 3))
-      (if (> (t n 2) limit)
-        (foldl + 0 (hash-table-keys acc))
-        (let subloop ((b 2))
-          (let ((v (t n b)))
-            (if (> v limit)
-              (loop (+ n 1))
-              (begin
-                (hash-table-set! acc v #t)
-                (subloop (+ b 1))))))))))
+    (do ((a 3 (+ a 1))) ((> (f a 2) n))
+      (do ((b 2 (+ b 1))
+           (v (f a 2)
+              (f a b)))
+        ((> v n))
+        (hash-table-set! acc v #t)))
+    (foldl + 1 (hash-table-keys acc))))
 
 (let ((_ (solve #e1e12)))
   (print _) (assert (= _ 336108797689259276)))
