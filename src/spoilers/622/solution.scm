cb1ca79e8366fe7a85ed02cd8f7a93dfd5fcf13b
turquoise-hexagon <camille@lzr.pw>
2023-02-26 14:05:05 +0000

    add 622

---
+++ src/spoilers/622/solution.scm
@@ -0,0 +1,33 @@
+(import
+  (euler))
+
+(define (_multiplicative-order a e p)
+  (let ((m (expt p e)) (t (* (- p 1) (expt p (- e 1)))))
+    (foldl
+      (lambda (acc _)
+        (apply
+          (lambda (e p)
+            (let loop ((i (modular-expt a (quotient t (expt p e)) m)) (acc acc))
+              (if (= i 1)
+                acc
+                (loop (modular-expt i p m) (* acc p)))))
+          _))
+      1 (run-length (factors t)))))
+
+(define (multiplicative-order a m)
+  (apply lcm
+    (map
+      (lambda (_)
+        (apply _multiplicative-order a _))
+      (run-length (factors m)))))
+
+(define (solve n)
+  (foldl
+    (lambda (acc i)
+      (if (= (multiplicative-order 2 i) n)
+        (+ acc i 1)
+        acc))
+    0 (divisors (- (expt 2 n) 1))))
+
+(let ((_ (solve 60)))
+  (print _) (assert (= _ 3010983666182123972)))
