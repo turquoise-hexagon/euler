3e22d986159e85e537dcf5b1bbe375843f41adea
turquoise-hexagon <camille@lzr.pw>
2023-02-28 04:00:24 +0000

    update 123

--- src/spoilers/123/solution.scm
+++ src/spoilers/123/solution.scm
@@ -1,21 +1,20 @@
 (import
   (euler))
 
-(define (helper prime index)
-  (let ((mod (* prime prime)))
-    (modulo (+ (modular-expt (- prime 1) index mod)
-               (modular-expt (+ prime 1) index mod))
-            mod)))
+(define-constant l #e5e5)
+
+(define (f p i)
+  (let ((m (* p p)))
+    (modulo
+      (+ (modular-expt (- p 1) i m)
+         (modular-expt (+ p 1) i m))
+      m)))
 
 (define (solve n)
-  (let ((primes (primes 1000000)))
-    (call/cc
-      (lambda (_)
-        (for-each
-          (lambda (prime index)
-            (when (> (helper prime index) n)
-              (_ index)))
-          primes (range 1 (length primes)))))))
+  (let loop ((p (primes l)) (i 1))
+    (if (> (f (car p) i) n)
+      i
+      (loop (cdr p) (+ i 1)))))
 
 (let ((_ (solve #e1e10)))
   (print _) (assert (= _ 21035)))
