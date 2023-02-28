5e4b99a08bc2e99803755c8cfc8df46e99dc8589
turquoise-hexagon <camille@lzr.pw>
2023-02-28 04:20:56 +0000

    update 092

--- src/092/solution.scm
+++ src/092/solution.scm
@@ -1,6 +1,5 @@
 (import
-  (chicken fixnum)
-  (srfi 69))
+  (chicken fixnum))
 
 (define (f n)
   (let loop ((n n) (acc 0))
@@ -10,26 +9,28 @@
         (let ((_ (fxmod n 10)))
           (fx+ acc (fx* _ _)))))))
 
-(define chain
-  (let ((cache (make-hash-table)))
-    ;; init cache
-    (hash-table-set! cache  1 #f)
-    (hash-table-set! cache 89 #t)
-    (lambda (n)
-      (if (hash-table-exists? cache n)
-        (hash-table-ref cache n)
-        (let ((_ (chain (f n))))
-          (hash-table-set! cache n _)
-          _)))))
+(define (make-valid? n)
+  (let ((acc (make-vector (fx+ n 1))))
+    (vector-set! acc 1  #f)
+    (vector-set! acc 89 #t)
+    (define (valid? n)
+      (let ((_ (vector-ref acc n)))
+        (if (boolean? _)
+          _
+          (let ((_ (valid? (f n))))
+            (vector-set! acc n _)
+            _))))
+    valid?))
 
 (define (solve n)
-  (let loop ((i 1) (acc 0))
-    (if (fx> i n)
-      acc
-      (loop (fx+ i 1)
-        (if (chain i)
-          (fx+ acc 1)
-          acc)))))
+  (let ((valid? (make-valid? n)))
+    (let loop ((i 1) (acc 0))
+      (if (fx> i n)
+        acc
+        (loop (fx+ i 1)
+          (if (valid? i)
+            (fx+ acc 1)
+            acc))))))
 
 (let ((_ (solve #e1e7)))
   (print _) (assert (= _ 8581146)))
