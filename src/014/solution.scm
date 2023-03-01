760a8ed8c5f4aa34b83301b9de90563a5929560d
turquoise-hexagon <camille@lzr.pw>
2023-03-01 21:17:08 +0000

    update 014

--- src/014/solution.scm
+++ src/014/solution.scm
@@ -1,15 +1,27 @@
 (import
   (chicken fixnum))
 
-(define (collatz n)
-  (let loop ((n n) (acc 1))
-    (if (fx= n 1)
-      acc
-      (loop
-        (if (fxodd? n)
-          (fx+ (fx* 3 n) 1)
-          (fx/ n 2))
-        (fx+ acc 1)))))
+(define-constant l #e5e6)
+
+(define-syntax next
+  (syntax-rules ()
+    ((_ n)
+     (if (fxodd? n)
+       (fx+ (fx* 3 n) 1)
+       (fx/ n 2)))))
+
+(define collatz
+  (let ((cache (make-vector (fx+ l 1) #f)))
+    (vector-set! cache 1 1)
+    (lambda (n)
+      (if (fx> n l)
+        (fx+ (collatz (next n)) 1)
+        (let ((acc (vector-ref cache n)))
+          (if acc
+            acc
+            (let ((acc (fx+ (collatz (next n)) 1)))
+              (vector-set! cache n acc)
+              acc)))))))
 
 (define (solve n)
   (let loop ((i 1) (res 0) (acc 0))
