2107a8a0325a314cec78d57a439a845c636adc83
turquoise-hexagon <camille@lzr.pw>
2023-03-02 01:08:44 +0000

    add 601

---
+++ src/spoilers/601/solution.scm
@@ -0,0 +1,15 @@
+(define (p s n)
+  (let loop ((i 1) (acc 1))
+    (if (> i s)
+      (- (floor (/ (- n 2) acc))
+         (floor (/ (- n 2) (lcm acc (+ s 1)))))
+      (loop (+ i 1) (lcm acc i)))))
+
+(define (solve n)
+  (let loop ((i 1) (acc 0))
+    (if (> i n)
+      acc
+      (loop (+ i 1) (+ acc (p i (expt 4 i)))))))
+
+(let ((_ (solve 31)))
+  (print _) (assert (= _ 1617243)))
