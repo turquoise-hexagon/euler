4fe2c444ad4748a81b8a7c87f4bf791f55a02075
turquoise-hexagon <camille@lzr.pw>
2023-03-15 04:53:59 +0000

    update 349

--- src/spoilers/349/solution.scm
+++ src/spoilers/349/solution.scm
@@ -12,7 +12,6 @@
 
 (define-constant h 100)
 (define-constant w 100)
-(define-constant center '(50 50))
 
 (define (array-init)
   (list->array (make-list h (make-list w #f))))
@@ -20,11 +19,12 @@
 (define (iterate-array! array dir coord)
   (let* ((len (length dirs)) (val (array-ref array coord)) (dir (modulo (+ dir (if val +1 -1) len) len)))
     (array-set! array coord (not val))
-    (values dir
+    (list
       (map
         (lambda (coord offset dim)
           (modulo (+ coord offset dim) dim))
-        coord (list-ref dirs dir) (array-dimensions array)))))
+        coord (list-ref dirs dir) (array-dimensions array))
+      dir)))
 
 (define (array-count array)
   (count
@@ -34,26 +34,23 @@
 
 (define (solve n)
   (let ((array (array-init)))
-    (let loop ((t 0) (dir 0) (coord center) (prev 0) (diff 0) (cnt 0))
+    (let loop ((t 0) (coord '(0 0)) (dir 0) (prev 0) (diff 0) (cnt 0))
       (if (= cnt 10)
         ;; cycle found, jump ahead
-        (let ((add (* (quotient (- n t) cycle) diff)))
-          (let loop ((t (+ (* (quotient (- n t) cycle) cycle) t)) (dir dir) (coord coord))
-            (if (= t n)
-              ;; compute result
-              (+ (array-count array) add)
-              (let-values (((dir coord) (iterate-array! array dir coord)))
-                (loop (+ t 1) dir coord)))))
+        (let loop ((i (+ (* (quotient (- n t) cycle) cycle) t)) (coord coord) (dir dir))
+          (if (= i n)
+            ;; compute result
+            (+ (array-count array) (* (quotient (- n t) cycle) diff))
+            (apply loop (+ i 1) (iterate-array! array dir coord))))
         ;; cycle not found, search further
-        (let subloop ((i 0) (dir dir) (coord coord))
+        (let subloop ((i 0) (coord coord) (dir dir))
           (if (= i cycle)
             (let* ((next-prev (array-count array)) (next-diff (- next-prev prev)))
-              (loop (+ t cycle) dir coord next-prev next-diff
-                (if (= diff next-diff)
+              (loop (+ t i) coord dir next-prev next-diff
+                (if (= next-diff diff)
                   (+ cnt 1)
                   0)))
-            (let-values (((dir coord) (iterate-array! array dir coord)))
-              (subloop (+ i 1) dir coord))))))))
+            (apply subloop (+ i 1) (iterate-array! array dir coord))))))))
 
 (let ((_ (solve #e1e18)))
   (print _) (assert (= _ 115384615384614952)))
