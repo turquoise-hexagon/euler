a62eb3eb19a241cf7c63bb54fafe6e8d50b0baee
turquoise-hexagon <camille@lzr.pw>
2023-04-02 17:49:38 +0100

    update 831

--- src/spoilers/831/solution.scm
+++ src/spoilers/831/solution.scm
@@ -3,12 +3,13 @@
   (srfi 152))
 
 (define (f n)
-  (* (+ (* 81   (expt n 4))
-        (* 684  (expt n 3))
-        (* 1401 (expt n 2))
-        (* 434  (expt n 1))
+  (* (+ (* 81   (expt n 5))
+        (* 765  (expt n 4))
+        (* 2085 (expt n 3))
+        (* 1835 (expt n 2))
+        (* 474  (expt n 1))
         40)
-     (expt 7 n) (+ n 1) 1/40))
+     (expt 7 n) 1/40))
 
 (define (output value base nb-digits)
   (string->number (substring (number->string value base) 0 nb-digits)))
