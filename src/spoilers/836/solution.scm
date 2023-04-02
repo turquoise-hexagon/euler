1732f5293b0ade0d2ea25a00ba92f93a93466ecb
turquoise-hexagon <camille@lzr.pw>
2023-04-02 16:52:06 +0100

    add 836

---
+++ src/spoilers/836/solution.scm
@@ -0,0 +1,19 @@
+(import
+  (chicken io)
+  (chicken irregex))
+
+(define-constant regex "<b>([a-z ]+)</b>")
+
+(define (import-input)
+  (flatten
+    (map
+      (lambda (str)
+        (irregex-split " " (irregex-match-substring (irregex-match regex str) 1)))
+      (irregex-extract regex (read-string)))))
+
+(define (solve input)
+  (list->string (map car (map string->list input))))
+
+(let ((input (import-input)))
+  (let ((_ (solve input)))
+    (print _) (assert (string=? _ "aprilfoolsjoke"))))
