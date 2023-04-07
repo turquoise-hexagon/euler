5fdabd37cc086848c09fa0447820962eb00ad5dd
turquoise-hexagon <camille@lzr.pw>
2023-04-07 10:35:02 +0100

    update 036

--- src/036/solution.scm
+++ src/036/solution.scm
@@ -1,21 +1,31 @@
 (import
-  (chicken fixnum))
+  (euler))
 
-(define (palindrome? n #!optional (b 10))
-  (let loop ((i n) (acc 0))
-    (if (fx= i 0)
-      (fx= n acc)
-      (loop (fx/ i b) (fx+ (fx* acc b) (fxmod i b))))))
-
-(define (solve n)
-  (let loop ((i 0) (acc 0))
-    (if (fx> i n)
+(define (palindrome n base bool)
+  (let loop ((n (if bool (quotient n base) n)) (acc n))
+    (if (= n 0)
       acc
-      (loop (fx+ i 1)
-        (if (and (palindrome? i)
-                 (palindrome? i 2))
-          (fx+ acc i)
-          acc)))))
+      (loop (quotient n base) (+ (* acc base) (modulo n base))))))
+
+(define (_palindromes limit base bool)
+  (let loop ((i 1) (acc '()))
+    (let ((_ (palindrome i base bool)))
+      (if (> _ limit)
+        acc
+        (loop (+ i 1) (cons _ acc))))))
+
+(define (palindromes limit base)
+  (append
+    (_palindromes limit base #t)
+    (_palindromes limit base #f)))
+
+(define (solve limit)
+  (foldl
+    (lambda (acc i)
+      (if (palindrome? i)
+        (+ acc i)
+        acc))
+    0 (palindromes limit 2)))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 872187)))
