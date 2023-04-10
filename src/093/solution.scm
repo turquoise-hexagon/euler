fab150129b920a0ed5220820c46596d6bd08dda0
turquoise-hexagon <camille@lzr.pw>
2023-04-10 11:37:44 +0100

    add 093

---
+++ src/093/solution.scm
@@ -0,0 +1,46 @@
+(import
+  (euler)
+  (srfi 69))
+
+(define operators (list + - * /))
+
+(define (delete-once lst item)
+  (let ((i (car lst)))
+    (if (= i item)
+      (cdr lst)
+      (cons i (delete-once (cdr lst) item)))))
+
+(define (run lst)
+  (let ((acc (make-hash-table)))
+    (let loop ((lst lst))
+      (if (and (null? (cdr lst)) (integer? (car lst)))
+        (hash-table-set! acc (car lst) #t)
+        (for-each
+          (lambda (pair)
+            (apply
+              (lambda (a b)
+                (let* ((lst (delete-once lst a))
+                       (lst (delete-once lst b)))
+                  (for-each
+                    (lambda (operator)
+                      (let ((i (operator a b)))
+                        (when (> i 0)
+                          (loop (cons i lst)))))
+                    operators)))
+              pair))
+          (let ((pairs (combinations lst 2)))
+            (append pairs (map reverse pairs))))))
+    acc))
+
+(define (value lst)
+  (let ((acc (run lst)))
+    (let loop ((i 1))
+      (if (hash-table-exists? acc i)
+        (loop (+ i 1))
+        (- i 1)))))
+
+(define (solve nb-digits)
+  (list->number (extremum (combinations (range 1 9) nb-digits) value >)))
+
+(let ((_ (solve 4)))
+  (print _) (assert (= _ 1258)))
