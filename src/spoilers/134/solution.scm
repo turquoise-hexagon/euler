108d5bfbf8246fe7fd45730e0783bb982bafabbe
turquoise-hexagon <camille@lzr.pw>
2023-04-29 06:43:51 +0100

    add 134

---
+++ src/spoilers/134/solution.scm
@@ -0,0 +1,27 @@
+(import
+  (euler))
+
+(define (next-prime p l n)
+  (let ((t (+ n 1)))
+    (if (< t l)
+      (vector-ref p t)
+      (do ((i (+ (vector-ref p n) 1) (+ i 1)))
+        ((prime? i) i)))))
+
+(define (order n)
+  (do ((i 1 (* i 10)))
+    ((> i n) i)))
+
+(define (function p l n)
+  (let ((a (vector-ref p n))
+        (b (next-prime p l n)))
+    (solve-chinese (list a 0) (list (order a) b))))
+
+(define (solve n)
+  (let* ((p (list->vector (list-tail (primes n) 2))) (l (vector-length p)))
+    (do ((i 0 (+ i 1))
+         (acc 0 (+ acc (function p l i))))
+      ((= i l) acc))))
+
+(let ((_ (solve #e1e6)))
+  (print _) (assert (= _ 18613426663617118)))
