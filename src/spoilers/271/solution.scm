828991adcb5d5dd31cddac87a7e992a804c203c2
turquoise-hexagon <camille@lzr.pw>
2023-05-14 04:12:48 +0100

    add 271

---
+++ src/spoilers/271/solution.scm
@@ -0,0 +1,21 @@
+(import
+  (euler))
+
+(define (s n)
+  (do ((i 1 (+ i 1))
+       (acc '() (if (= (modular-expt i 3 n) 1)
+                  (cons i acc)
+                  acc)))
+    ((> i n) acc)))
+
+(define (solve n)
+  (let ((factors (factors n)))
+    (- (apply +
+         (map
+           (lambda (solution)
+             (solve-chinese solution factors))
+           (apply product (map s factors))))
+       1)))
+
+(let ((_ (solve 13082761331670030)))
+  (print _) (assert (= _ 4617456485273129588)))
