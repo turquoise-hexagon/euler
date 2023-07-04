4ee851232c8e27635d0b9e920ec3b3e61dfafda0
turquoise-hexagon <camille@lzr.pw>
2023-07-04 09:32:00 +0100

    update 060

--- src/060/solution.scm
+++ src/060/solution.scm
@@ -1,61 +1,86 @@
 (import
+  (chicken fixnum)
   (euler)
-  (only
-    (srfi 1)
-    every
-    filter))
+  (srfi 69)
+  (except (srfi 1) concatenate))
 
-(define-constant LIMIT #e1e4)
+(define-constant limit 10000)
 
 (define (concatenate a b)
-  (let loop ((i b) (a a))
-    (if (= i 0)
-      (+ a b)
-      (loop (quotient i 10) (* a 10)))))
+  (do ((i b (fx/ i 10))
+       (a a (fx* a 10)))
+    ((fx= i 0) (fx+ a b))))
 
-(define (make-prime? n)
-  (let ((acc (make-vector (+ n 1) #f)))
+(define (make-prime? limit)
+  (let ((acc (make-vector limit #f)))
     (for-each
-      (lambda (_)
-        (vector-set! acc _ #t))
-      (primes n))
-    (define (prime? n)
-      (vector-ref acc n))
-    prime?))
+      (lambda (i)
+        (vector-set! acc i #t))
+      (primes limit))
+    (lambda (n)
+      (vector-ref acc n))))
 
-(define (make-group n)
-  (let ((prime? (make-prime? (* n n))))
-    (define (group lst primes)
-      (foldl
-        (lambda (acc p)
-          (map
-            (lambda (group)
-              (if (every
-                    (lambda (i)
-                      (and
-                        (prime? (concatenate i p))
-                        (prime? (concatenate p i))))
-                    group)
-                (cons p group)
-                group))
-            acc))
-        lst primes))
-    group))
+(define (make-should-be-connected? limit)
+  (let ((prime? (make-prime? (fx* limit limit))))
+    (lambda (a b)
+      (and
+        (prime? (concatenate a b))
+        (prime? (concatenate b a))))))
 
-(define (solve n)
-  (let ((primes (primes LIMIT)) (group (make-group LIMIT)))
-    (let loop ((acc (map list primes)))
-      (let ((_ (group acc primes)))
-        (if (equal? _ acc)
-          (apply min
-            (map
-              (lambda (group)
-                (apply + group))
+(define (connect! graph a b)
+  (let ((acc (hash-table-ref/default graph a (make-hash-table))))
+    (hash-table-set! acc b #t)
+    (hash-table-set! graph a acc)))
+
+(define (graph limit)
+  (let ((acc (make-hash-table)) (should-be-connected? (make-should-be-connected? limit)))
+    (for-each
+      (lambda (pair)
+        (apply
+          (lambda (a b)
+            (when (should-be-connected? a b)
+              (connect! acc a b)
+              (connect! acc b a)))
+          pair))
+      (combinations (primes limit) 2))
+    acc))
+
+(define (find-map fun lst)
+  (let loop ((lst lst))
+    (if (null? lst)
+      #f
+      (let ((acc (fun (car lst))))
+        (if acc
+          acc
+          (loop (cdr lst)))))))
+
+(define (connected? graph a b)
+  (hash-table-exists? (hash-table-ref graph a) b))
+
+(define (would-be-clique? graph node clique)
+  (every
+    (lambda (i)
+      (connected? graph i node))
+    clique))
+
+(define (clique graph size)
+  (find-map
+    (lambda (node)
+      (let loop ((node node) (len 0) (acc '()))
+        (if (fx= len size)
+          acc
+          (let ((len (fx+ len 1)))
+            (find-map
+              (lambda (node)
+                (loop node len (cons node acc)))
               (filter
-                (lambda (group)
-                  (= (length group) n))
-                acc)))
-          (loop _))))))
+                (lambda (node)
+                  (would-be-clique? graph node acc))
+                (hash-table-keys (hash-table-ref graph node))))))))
+    (hash-table-keys graph)))
+
+(define (solve size)
+  (foldl fx+ 0 (clique (graph limit) size)))
 
 (let ((_ (solve 5)))
   (print _) (assert (= _ 26033)))
