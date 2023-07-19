04d9389546e2b7637a6f5ab515eb078406090be8
turquoise-hexagon <camille@lzr.pw>
2023-07-19 15:02:25 +0100

    add 088

---
+++ src/088/solution.scm
@@ -0,0 +1,21 @@
+(import
+  (chicken fixnum)
+  (srfi 69))
+
+(define (solve l)
+  (let ((m (make-vector (fx+ l 1) most-positive-fixnum)))
+
+    (let loop ((a 1) (b 1) (c 1) (d 2))
+      (let ((_ (fx+ (fx- a b) c)))
+        (unless (fx> _ l)
+          (vector-set! m _ (fxmin a (vector-ref m _)))
+          (let ((_ (fx* (fx/ l a) 2)))
+            (do ((i d (fx+ i 1))) ((fx> i _))
+              (loop (fx* a i) (fx+ b i) (fx+ c 1) i))))))
+
+    (let ((t (make-hash-table)))
+      (do ((i 2 (fx+ i 1))) ((fx> i l))
+        (hash-table-set! t (vector-ref m i) #t))
+      (foldl fx+ 0 (hash-table-keys t)))))
+
+(print (solve 12000))
