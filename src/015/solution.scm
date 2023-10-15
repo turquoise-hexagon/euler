be3c7243b8572e3df9327934f6285170d2e7fe07
turquoise-hexagon <camille@lzr.pw>
2023-10-15 15:14:38 +0100

    update 015

--- src/015/solution.scm
+++ src/015/solution.scm
@@ -1,11 +1,8 @@
 (import
   (euler))
 
-(define (choose a b)
-  (/ (factorial a) (* (factorial b) (factorial (- a b)))))
-
 (define (solve n)
-  (choose (* 2 n) n))
+  (binomial (* 2 n) n))
 
 (let ((_ (solve 20)))
   (print _) (assert (= _ 137846528820)))
