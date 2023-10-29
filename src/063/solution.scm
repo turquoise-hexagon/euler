bd19b3b35d3e872da73d50b21a118d2ea954c6dc
turquoise-hexagon <camille@lzr.pw>
2023-10-29 13:38:04 +0000

    update 063

--- src/063/solution.scm
+++ src/063/solution.scm
@@ -2,16 +2,10 @@
   (euler)
   (srfi 1))
 
-(define (number-length n)
-  (let loop ((i n) (acc 0))
-    (if (= i 0)
-      acc
-      (loop (quotient i 10) (+ acc 1)))))
-
 (define bound/1
   (let loop ((i 1))
     (let ((_ (+ i 1)))
-      (if (> (number-length (expt _ _)) _)
+      (if (> (integer-log (expt _ _)) _)
         i
         (loop _)))))
 
@@ -21,7 +15,7 @@
       (lambda (n)
         (let loop ((i 1))
           (let ((_ (+ i 1)))
-            (if (> (number-length (expt n _)) bound/1)
+            (if (> (integer-log (expt n _)) bound/1)
               i
               (loop _)))))
       (range 2 bound/1))))
@@ -32,7 +26,7 @@
       (apply
         (lambda (i power)
           (let ((_ (expt i power)))
-            (= (number-length _) power)))
+            (= (integer-log _) power)))
         pair))
     (product (range 1 bound/1) (range 1 bound/2))))
 
