5604ef9b8d0077beacdd08259de9d9ec8395c8f1
turquoise-hexagon <camille@lzr.pw>
2023-10-29 13:36:20 +0000

    update 057

--- src/057/solution.scm
+++ src/057/solution.scm
@@ -1,17 +1,14 @@
 (import
   (euler))
 
-(define (number-length n)
-  (length (number->list n)))
-
 (define (solve n)
   (let loop ((num 3) (den 2) (i 0) (acc 0))
     (if (> i n)
       acc
       (let* ((num (+ num den den)) (den (- num den)))
         (loop num den (+ i 1)
-          (if (> (number-length num)
-                 (number-length den))
+          (if (> (integer-log num)
+                 (integer-log den))
             (+ acc 1)
             acc))))))
 
