efc9a6c30764f7aea1291946b1c3a695d30e3adf
turquoise-hexagon <camille@lzr.pw>
2023-10-29 13:37:30 +0000

    update 040

--- src/040/solution.scm
+++ src/040/solution.scm
@@ -1,14 +1,9 @@
 (import
+  (euler)
   (chicken fixnum))
 
-(define (number-length n)
-  (let loop ((n n) (acc 0))
-    (if (fx= n 0)
-      acc
-      (loop (fx/ n 10) (fx+ acc 1)))))
-
 (define (nth-digit n index)
-  (let loop ((n n) (index (fx- (number-length n) index)))
+  (let loop ((n n) (index (fx- (integer-log n) index)))
     (if (fx= index 0)
       (fxmod n 10)
       (loop (fx/ n 10) (fx- index 1)))))
@@ -17,7 +12,7 @@
   (let loop ((i 1) (index 1) (counted 0) (acc 1))
     (if (fx> index n)
       acc
-      (let* ((len (number-length i)) (total (fx+ counted len)))
+      (let* ((len (integer-log i)) (total (fx+ counted len)))
         (if (fx< total index)
           (loop (fx+ i 1) index total acc)
           (loop (fx+ i 1) (fx* index 10) total (fx* acc (nth-digit i (fx- len (fx- total index))))))))))
