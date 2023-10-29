9c5be447775015742788087a2af31909f930c2ca
turquoise-hexagon <camille@lzr.pw>
2023-10-29 13:35:41 +0000

    update 013

--- src/013/solution.scm
+++ src/013/solution.scm
@@ -1,14 +1,12 @@
 (import
+  (euler)
   (chicken io))
 
 (define (import-input)
   (apply + (map string->number (read-lines))))
 
-(define (number-length n)
-  (inexact->exact (ceiling (log n 10))))
-
 (define (solve input)
-  (quotient input (expt 10 (- (number-length input) 10))))
+  (quotient input (expt 10 (- (integer-log input) 10))))
 
 (let ((_ (solve (import-input))))
   (print _) (assert (= _ 5537376230)))
