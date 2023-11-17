a42341c7081412b092acf06ab59b9000ac3a052c
turquoise-hexagon <camille@lzr.pw>
2023-11-17 00:26:52 +0000

    update 013

--- src/013/solution.scm
+++ src/013/solution.scm
@@ -6,7 +6,7 @@
   (apply + (map string->number (read-lines))))
 
 (define (solve input)
-  (quotient input (expt 10 (- (integer-log input) 10))))
+  (quotient input (expt 10 (- (+ (integer-log input) 1) 10))))
 
 (let ((_ (solve (import-input))))
   (print _) (assert (= _ 5537376230)))
