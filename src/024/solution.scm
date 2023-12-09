c6291456a2a368969d9c83ecb84a299efe9f3bbf
turquoise-hexagon <camille@lzr.pw>
2023-12-09 08:42:30 +0000

    update 024

--- src/024/solution.scm
+++ src/024/solution.scm
@@ -9,7 +9,7 @@
         (cons (list-ref l q) (loop (delete-at l q) (- i 1) r))))))
 
 (define (solve n)
-  (list->number (nth-permutation (range 0 9) n)))
+  (list->number (nth-permutation (range 9) n)))
 
 (let ((_ (solve #e1e6)))
   (print _) (assert (= _ 2783915460)))
