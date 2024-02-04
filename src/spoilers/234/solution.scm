4a4e2e52ca0835b1d4b1ae5580de1d5dd2731df6
turquoise-hexagon <camille@lzr.pw>
2024-02-04 00:48:37 +0000

    add 234

---
+++ src/spoilers/234/solution.scm
@@ -0,0 +1,28 @@
+(import
+  (euler)
+  (srfi 1)
+  (srfi 69))
+
+(define (next-prime n)
+  (do ((i (+ n 1) (+ i 1)))
+    ((prime? i) i)))
+
+(define (solve limit)
+  (let ((lst (primes (next-prime (fxsqrt limit)))) (acc (make-hash-table #:initial 0)))
+    (for-each
+      (lambda (a b)
+        (let ((l (* a a))
+              (h (* b b)))
+          (do ((i (+ l a) (+ i a))) ((> i h)) (hash-table-update! acc i add1))
+          (do ((i (- h b) (- i b))) ((< i l)) (hash-table-update! acc i add1))))
+      (list-tail lst 0)
+      (list-tail lst 1))
+    (hash-table-fold acc
+      (lambda (i cnt acc)
+        (if (and (= cnt 1) (<= i limit))
+          (+ acc i)
+          acc))
+      0)))
+
+(let ((_ (solve 999966663333)))
+  (print _) (assert (= _ 1259187438574927161)))
