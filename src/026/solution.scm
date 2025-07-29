ec34943571f6aeecb2de413445e5d2d126c7b871
turquoise-hexagon <camille@lzr.pw>
2025-07-29 16:47:23 +0100

    update 026

--- src/026/solution.scm
+++ src/026/solution.scm
@@ -1,16 +1,19 @@
 (import
   (euler)
-  (srfi 1))
+  (srfi 69))
 
-(define (solve n)
-  (extremum
-    (filter
-      (lambda (i)
-        (= (gcd 10 i) 1))
-      (range 2 n))
-    (lambda (i)
-      (discrete-log 10 1 i))
-    >))
+(define (cycle n)
+  (let ((mem (make-hash-table)))
+    (let loop ((i (expt 10 (integer-log n))))
+      (let ((i (modulo i n)))
+        (if (hash-table-exists? mem i)
+          (hash-table-size mem)
+          (begin
+            (hash-table-set! mem i #t)
+            (loop (* i 10))))))))
+
+(define (solve limit)
+  (extremum (range 2 limit) cycle >))
 
 (let ((_ (solve 1000)))
   (print _) (assert (= _ 983)))
