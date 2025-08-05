d0be18ccc885adadbe8b67d1d2f8b16e1030172b
turquoise-hexagon <camille@lzr.pw>
2025-08-05 04:16:38 +0100

    update 932

--- src/spoilers/932/solution.scm
+++ src/spoilers/932/solution.scm
@@ -1,19 +1,14 @@
 (import
   (chicken fixnum))
 
-(define (concatenate a b)
-  (do ((a a (fx* a 10))
-       (i b (fx/ i 10)))
-    ((fx= i 0)
-     (fx+ a b))))
-
 (define (valid? n)
-  (let loop ((a n) (b 0) (m 1))
+  (let loop ((a n) (b 0) (m 1) (t #f))
     (if (fx= a 0)
       #f
-      (if (and (let ((_ (fx+ a b))) (fx= (fx* _ _) n)) (fx= (concatenate a b) n))
+      (if (and t (fx= (fx* (fx+ a b) (fx+ a b)) n))
         #t
-        (loop (fx/ a 10) (fx+ (fx* (fxmod a 10) m) b) (fx* m 10))))))
+        (let ((d (fxmod a 10)))
+          (loop (fx/ a 10) (fx+ (fx* d m) b) (fx* m 10) (fx> d 0)))))))
 
 (define (solve nb-digits)
   (let ((l (expt 10 nb-digits)))
